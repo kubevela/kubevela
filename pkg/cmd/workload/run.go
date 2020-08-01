wonderflow/RudrX

refactor rudr run to rudr <workload>:run

pkg/cmd/workload/run.go
@@ -1,0 +1,132 @@
+package workload
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cloud-native-application/rudrx/pkg/plugins"
+
+	"github.com/cloud-native-application/rudrx/pkg/cmd"
+
+	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
+	corev1alpha2 "github.com/crossplane/oam-kubernetes-runtime/apis/core/v1alpha2"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	cmdutil "github.com/cloud-native-application/rudrx/pkg/cmd/util"
+)
+
+type runOptions struct {
+	Template  cmdutil.Template
+	Env       *cmd.EnvMeta
+	Component corev1alpha2.Component
+	AppConfig corev1alpha2.ApplicationConfiguration
+	client    client.Client
+	cmdutil.IOStreams
+}
+
+func newRunOptions(ioStreams cmdutil.IOStreams) *runOptions {
+	return &runOptions{IOStreams: ioStreams}
+}
+
+func AddPlugins(parentCmd *cobra.Command, c client.Client, ioStreams cmdutil.IOStreams) error {
+	templates, err := plugins.GetWorkloadsFromCluster(context.TODO(), c)
+	if err != nil {
+		return err
+	}
+
+	for _, tmp := range templates {
+		var name string
+		if tmp.Alias != "" {
+			name = tmp.Alias
+		}
+		o := newRunOptions(ioStreams)
+		o.client = c
+		o.Env, _ = cmd.GetEnv()
+		pluginCmd := &cobra.Command{
+			Use:                   name + ":run [args]",
+			DisableFlagsInUseLine: true,
+			Short:                 "Run " + name + " workloads",
+			Long:                  "Run " + name + " workloads",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				if err := o.Complete(cmd, args, context.TODO()); err != nil {
+					return err
+				}
+				return o.Run(cmd)
+			},
+		}
+		pluginCmd.SetOut(o.Out)
+		for _, v := range tmp.Parameters {
+			pluginCmd.Flags().StringP(v.Name, v.Short, v.Default, v.Usage)
+			if v.Required {
+				pluginCmd.MarkFlagRequired(v.Name)
+			}
+		}
+
+		o.Template = tmp
+		parentCmd.AddCommand(pluginCmd)
+	}
+	return nil
+}
+
+func (o *runOptions) Complete(cmd *cobra.Command, args []string, ctx context.Context) error {
+
+	argsLength := len(args)
+
+	if argsLength < 1 {
+		return errors.New("must specify name for workload")
+	}
+
+	workloadName := args[0]
+
+	workloadTemplate := o.Template
+	pvd := fieldpath.Pave(workloadTemplate.Object.Object)
+	for _, v := range workloadTemplate.Parameters {
+		var paraV string
+
+		flagSet := cmd.Flag(v.Name)
+		paraV = flagSet.Value.String()
+
+		if paraV == "" {
+			continue
+		}
+
+		for _, path := range v.FieldPaths {
+			if v.Type == "int" {
+				portValue, _ := strconv.ParseFloat(paraV, 64)
+				pvd.SetNumber(path, portValue)
+				break
+			}
+			pvd.SetString(path, paraV)
+		}
+	}
+
+	pvd.SetString("metadata.name", strings.ToLower(workloadName))
+	namespace := o.Env.Namespace
+	o.Component.Spec.Workload.Object = &unstructured.Unstructured{Object: pvd.UnstructuredContent()}
+	o.Component.Name = args[0]
+	o.Component.Namespace = namespace
+	o.AppConfig.Name = args[0]
+	o.AppConfig.Namespace = namespace
+	o.AppConfig.Spec.Components = append(o.AppConfig.Spec.Components, corev1alpha2.ApplicationConfigurationComponent{ComponentName: args[0]})
+
+	return nil
+}
+
+func (o *runOptions) Run(cmd *cobra.Command) error {
+	o.Infof("Creating AppConfig %s\n", o.AppConfig.Name)
+	err := o.client.Create(context.Background(), &o.Component)
+	if err != nil {
+		return fmt.Errorf("create component err: %s", err)
+	}
+	err = o.client.Create(context.Background(), &o.AppConfig)
+	if err != nil {
+		return fmt.Errorf("create appconfig err %s", err)
+	}
+	o.Info("SUCCEED")
+	return nil
+}

pkg/plugins/local.go
@@ -1,0 +1,61 @@
+package plugins
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+
+	cmdutil "github.com/cloud-native-application/rudrx/pkg/cmd/util"
+)
+
+func GetDefFromLocal(dir string, defType cmdutil.DefinitionType) ([]cmdutil.Template, error) {
+	temps, err := LoadTempFromLocal(dir)
+	if err != nil {
+		return nil, err
+	}
+	var defs []cmdutil.Template
+	for _, t := range temps {
+		if t.Type != defType {
+			continue
+		}
+		defs = append(defs, t)
+	}
+	return defs, nil
+}
+
+func SinkTemp2Local(templates []cmdutil.Template, dir string) error {
+	for _, tmp := range templates {
+		data, err := json.Marshal(tmp)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(filepath.Join(dir, tmp.Name), data, 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func LoadTempFromLocal(dir string) ([]cmdutil.Template, error) {
+	var tmps []cmdutil.Template
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		data, err := ioutil.ReadFile(filepath.Join(dir, f.Name()))
+		if err != nil {
+			return nil, err
+		}
+		var tmp cmdutil.Template
+		if err = json.Unmarshal(data, &tmp); err != nil {
+			return nil, err
+		}
+		tmps = append(tmps, tmp)
+	}
+	return tmps, nil
+}

pkg/plugins/local_test.go
@@ -1,0 +1,125 @@
+package plugins
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/cloud-native-application/rudrx/pkg/cmd/util"
+)
+
+func TestLocalSink(t *testing.T) {
+	deployment := util.Template{
+		Name:  "deployment",
+		Type:  util.TypeWorkload,
+		Alias: "deployment",
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Deployment",
+		},
+		Parameters: []util.Parameter{
+			{
+				Name:       "image",
+				Short:      "i",
+				Required:   true,
+				FieldPaths: []string{"spec.containers[0].image"},
+			},
+		},
+	}
+	statefulset := util.Template{
+		Name:  "statefulset",
+		Type:  util.TypeWorkload,
+		Alias: "stateful",
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Statefulset",
+		},
+		Parameters: []util.Parameter{
+			{
+				Name:       "image",
+				Short:      "i",
+				Required:   true,
+				FieldPaths: []string{"spec.containers[0].image"},
+			},
+		},
+	}
+	route := util.Template{
+		Name:  "route",
+		Type:  util.TypeTrait,
+		Alias: "route",
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Route",
+		},
+		Parameters: []util.Parameter{
+			{
+				Name:       "domain",
+				Short:      "d",
+				Required:   true,
+				FieldPaths: []string{"spec.domain"},
+			},
+		},
+	}
+
+	cases := map[string]struct {
+		dir    string
+		tmps   []util.Template
+		Type   util.DefinitionType
+		expDef []util.Template
+	}{
+		"Test No Templates": {
+			dir:  "rudrx-test1",
+			tmps: nil,
+		},
+		"Test Only Workload": {
+			dir:    "rudrx-test2",
+			tmps:   []util.Template{deployment, statefulset},
+			Type:   util.TypeWorkload,
+			expDef: []util.Template{deployment, statefulset},
+		},
+		"Test Only Trait": {
+			dir:    "rudrx-test3",
+			tmps:   []util.Template{route},
+			Type:   util.TypeTrait,
+			expDef: []util.Template{route},
+		},
+		"Test Only Workload But want trait": {
+			dir:    "rudrx-test3",
+			tmps:   []util.Template{deployment, statefulset},
+			Type:   util.TypeTrait,
+			expDef: nil,
+		},
+		"Test Both have Workload and trait But want Workload": {
+			dir:    "rudrx-test4",
+			tmps:   []util.Template{deployment, route, statefulset},
+			Type:   util.TypeWorkload,
+			expDef: []util.Template{deployment, statefulset},
+		},
+		"Test Both have Workload and trait But want Trait": {
+			dir:    "rudrx-test5",
+			tmps:   []util.Template{deployment, route, statefulset},
+			Type:   util.TypeTrait,
+			expDef: []util.Template{route},
+		},
+	}
+	for name, c := range cases {
+		testInDir(t, name, c.dir, c.tmps, c.expDef, c.Type)
+	}
+}
+
+func testInDir(t *testing.T, casename, dir string, tmps, defexp []util.Template, Type util.DefinitionType) {
+	err := os.MkdirAll(dir, 0755)
+	assert.NoError(t, err, casename)
+	defer os.RemoveAll(dir)
+	err = SinkTemp2Local(tmps, dir)
+	assert.NoError(t, err, casename)
+	gottmps, err := LoadTempFromLocal(dir)
+	assert.NoError(t, err, casename)
+	assert.Equal(t, tmps, gottmps, casename)
+	if Type != "" {
+		gotDef, err := GetDefFromLocal(dir, Type)
+		assert.NoError(t, err, casename)
+		assert.Equal(t, defexp, gotDef, casename)
+	}
+}

kubevela/kubevela

pkg/cmd/workload/run.go
@@ -1,0 +1,134 @@
+package workload
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cloud-native-application/rudrx/api/types"
+
+	"github.com/cloud-native-application/rudrx/pkg/plugins"
+
+	"github.com/cloud-native-application/rudrx/pkg/cmd"
+
+	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
+	corev1alpha2 "github.com/crossplane/oam-kubernetes-runtime/apis/core/v1alpha2"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	cmdutil "github.com/cloud-native-application/rudrx/pkg/cmd/util"
+)
+
+type runOptions struct {
+	Template  types.Template
+	Env       *cmd.EnvMeta
+	Component corev1alpha2.Component
+	AppConfig corev1alpha2.ApplicationConfiguration
+	client    client.Client
+	cmdutil.IOStreams
+}
+
+func newRunOptions(ioStreams cmdutil.IOStreams) *runOptions {
+	return &runOptions{IOStreams: ioStreams}
+}
+
+func AddPlugins(parentCmd *cobra.Command, c client.Client, ioStreams cmdutil.IOStreams) error {
+	templates, err := plugins.GetWorkloadsFromCluster(context.TODO(), types.DefaultOAMNS, c)
+	if err != nil {
+		return err
+	}
+
+	for _, tmp := range templates {
+		var name string
+		if tmp.Alias != "" {
+			name = tmp.Alias
+		}
+		o := newRunOptions(ioStreams)
+		o.client = c
+		o.Env, _ = cmd.GetEnv()
+		pluginCmd := &cobra.Command{
+			Use:                   name + ":run [args]",
+			DisableFlagsInUseLine: true,
+			Short:                 "Run " + name + " workloads",
+			Long:                  "Run " + name + " workloads",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				if err := o.Complete(cmd, args, context.TODO()); err != nil {
+					return err
+				}
+				return o.Run(cmd)
+			},
+		}
+		pluginCmd.SetOut(o.Out)
+		for _, v := range tmp.Parameters {
+			pluginCmd.Flags().StringP(v.Name, v.Short, v.Default, v.Usage)
+			if v.Required {
+				pluginCmd.MarkFlagRequired(v.Name)
+			}
+		}
+
+		o.Template = tmp
+		parentCmd.AddCommand(pluginCmd)
+	}
+	return nil
+}
+
+func (o *runOptions) Complete(cmd *cobra.Command, args []string, ctx context.Context) error {
+
+	argsLength := len(args)
+
+	if argsLength < 1 {
+		return errors.New("must specify name for workload")
+	}
+
+	workloadName := args[0]
+
+	workloadTemplate := o.Template
+	pvd := fieldpath.Pave(workloadTemplate.Object)
+	for _, v := range workloadTemplate.Parameters {
+		var paraV string
+
+		flagSet := cmd.Flag(v.Name)
+		paraV = flagSet.Value.String()
+
+		if paraV == "" {
+			continue
+		}
+
+		for _, path := range v.FieldPaths {
+			if v.Type == "int" {
+				portValue, _ := strconv.ParseFloat(paraV, 64)
+				pvd.SetNumber(path, portValue)
+				break
+			}
+			pvd.SetString(path, paraV)
+		}
+	}
+
+	pvd.SetString("metadata.name", strings.ToLower(workloadName))
+	namespace := o.Env.Namespace
+	o.Component.Spec.Workload.Object = &unstructured.Unstructured{Object: pvd.UnstructuredContent()}
+	o.Component.Name = args[0]
+	o.Component.Namespace = namespace
+	o.AppConfig.Name = args[0]
+	o.AppConfig.Namespace = namespace
+	o.AppConfig.Spec.Components = append(o.AppConfig.Spec.Components, corev1alpha2.ApplicationConfigurationComponent{ComponentName: args[0]})
+
+	return nil
+}
+
+func (o *runOptions) Run(cmd *cobra.Command) error {
+	o.Infof("Creating AppConfig %s\n", o.AppConfig.Name)
+	err := o.client.Create(context.Background(), &o.Component)
+	if err != nil {
+		return fmt.Errorf("create component err: %s", err)
+	}
+	err = o.client.Create(context.Background(), &o.AppConfig)
+	if err != nil {
+		return fmt.Errorf("create appconfig err %s", err)
+	}
+	o.Info("SUCCEED")
+	return nil
+}

pkg/plugins/local.go
@@ -1,0 +1,61 @@
+package plugins
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/cloud-native-application/rudrx/api/types"
+)
+
+func GetDefFromLocal(dir string, defType types.DefinitionType) ([]types.Template, error) {
+	temps, err := LoadTempFromLocal(dir)
+	if err != nil {
+		return nil, err
+	}
+	var defs []types.Template
+	for _, t := range temps {
+		if t.Type != defType {
+			continue
+		}
+		defs = append(defs, t)
+	}
+	return defs, nil
+}
+
+func SinkTemp2Local(templates []types.Template, dir string) error {
+	for _, tmp := range templates {
+		data, err := json.Marshal(tmp)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(filepath.Join(dir, tmp.Name), data, 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func LoadTempFromLocal(dir string) ([]types.Template, error) {
+	var tmps []types.Template
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		data, err := ioutil.ReadFile(filepath.Join(dir, f.Name()))
+		if err != nil {
+			return nil, err
+		}
+		var tmp types.Template
+		if err = json.Unmarshal(data, &tmp); err != nil {
+			return nil, err
+		}
+		tmps = append(tmps, tmp)
+	}
+	return tmps, nil
+}

pkg/plugins/local_test.go
@@ -1,0 +1,125 @@
+package plugins
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cloud-native-application/rudrx/api/types"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocalSink(t *testing.T) {
+	deployment := types.Template{
+		Name:  "deployment",
+		Type:  types.TypeWorkload,
+		Alias: "deployment",
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Deployment",
+		},
+		Parameters: []types.Parameter{
+			{
+				Name:       "image",
+				Short:      "i",
+				Required:   true,
+				FieldPaths: []string{"spec.containers[0].image"},
+			},
+		},
+	}
+	statefulset := types.Template{
+		Name:  "statefulset",
+		Type:  types.TypeWorkload,
+		Alias: "stateful",
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Statefulset",
+		},
+		Parameters: []types.Parameter{
+			{
+				Name:       "image",
+				Short:      "i",
+				Required:   true,
+				FieldPaths: []string{"spec.containers[0].image"},
+			},
+		},
+	}
+	route := types.Template{
+		Name:  "route",
+		Type:  types.TypeTrait,
+		Alias: "route",
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Route",
+		},
+		Parameters: []types.Parameter{
+			{
+				Name:       "domain",
+				Short:      "d",
+				Required:   true,
+				FieldPaths: []string{"spec.domain"},
+			},
+		},
+	}
+
+	cases := map[string]struct {
+		dir    string
+		tmps   []types.Template
+		Type   types.DefinitionType
+		expDef []types.Template
+	}{
+		"Test No Templates": {
+			dir:  "rudrx-test1",
+			tmps: nil,
+		},
+		"Test Only Workload": {
+			dir:    "rudrx-test2",
+			tmps:   []types.Template{deployment, statefulset},
+			Type:   types.TypeWorkload,
+			expDef: []types.Template{deployment, statefulset},
+		},
+		"Test Only Trait": {
+			dir:    "rudrx-test3",
+			tmps:   []types.Template{route},
+			Type:   types.TypeTrait,
+			expDef: []types.Template{route},
+		},
+		"Test Only Workload But want trait": {
+			dir:    "rudrx-test3",
+			tmps:   []types.Template{deployment, statefulset},
+			Type:   types.TypeTrait,
+			expDef: nil,
+		},
+		"Test Both have Workload and trait But want Workload": {
+			dir:    "rudrx-test4",
+			tmps:   []types.Template{deployment, route, statefulset},
+			Type:   types.TypeWorkload,
+			expDef: []types.Template{deployment, statefulset},
+		},
+		"Test Both have Workload and trait But want Trait": {
+			dir:    "rudrx-test5",
+			tmps:   []types.Template{deployment, route, statefulset},
+			Type:   types.TypeTrait,
+			expDef: []types.Template{route},
+		},
+	}
+	for name, c := range cases {
+		testInDir(t, name, c.dir, c.tmps, c.expDef, c.Type)
+	}
+}
+
+func testInDir(t *testing.T, casename, dir string, tmps, defexp []types.Template, Type types.DefinitionType) {
+	err := os.MkdirAll(dir, 0755)
+	assert.NoError(t, err, casename)
+	defer os.RemoveAll(dir)
+	err = SinkTemp2Local(tmps, dir)
+	assert.NoError(t, err, casename)
+	gottmps, err := LoadTempFromLocal(dir)
+	assert.NoError(t, err, casename)
+	assert.Equal(t, tmps, gottmps, casename)
+	if Type != "" {
+		gotDef, err := GetDefFromLocal(dir, Type)
+		assert.NoError(t, err, casename)
+		assert.Equal(t, defexp, gotDef, casename)
+	}
+}