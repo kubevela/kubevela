7272069bff4bf05be6cda19ae7f37c5955a118d0
zzxwill <zzxwill@gmail.com>
2020-08-04 21:19:06 +0800

    remove old detach command from main

--- cmd/rudrx/main.go
+++ cmd/rudrx/main.go
@@ -88,7 +88,6 @@ func newCommand() *cobra.Command {
 		cmd.NewEnvSwitchCommand(f, ioStream),
 		cmd.NewEnvDeleteCommand(f, ioStream),
 		cmd.NewEnvCommand(f, ioStream),
-		cmd.NewDetachCommand(f, client, ioStream),
 		NewVersionCommand(),
 		cmd.NewAppStatusCommand(client, ioStream),
 	)
