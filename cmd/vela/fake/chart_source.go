0b1aab76d4ef2518d06f3e1e066fd9e2dfcbc181
天元 <jianbo.sjb@alibaba-inc.com>
2020-10-13 10:31:25 +0800

    add chart_source

---
+++ cmd/vela/fake/chart_source.go
@@ -0,0 +1,5 @@
+package fake
+
+// ChartSource is a base64-encoded, gzipped tarball of the charts/vela-core dir.
+// This whole file will be rewrite at build time, please don't change this file.
+var ChartSource string
