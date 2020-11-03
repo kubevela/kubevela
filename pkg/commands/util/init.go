5066320a2f56ecb0ee1f56370395c287e1a852ae
天元 <jianbo.sjb@alibaba-inc.com>
2020-11-03 10:25:57 +0800

    start promethus instance on installation
    Signed-off-by: 天元 <jianbo.sjb@alibaba-inc.com>

--- pkg/commands/util/init.go
+++ pkg/commands/util/init.go
@@ -2,8 +2,6 @@ package util
 
 import (
 	"context"
-	"os"
-	"path/filepath"
 
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
@@ -11,38 +9,10 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// GetKubeClient creates a Kubernetes config and client for a given kubeconfig context.
-func GetKubeClient() error {
-
-	config, err := clientcmd.BuildConfigFromFlags("", GetKubeConfig())
-	if err != nil {
-		return err
-	}
-
-	// Creates the dynamic interface.
-	_, err = kubernetes.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func HomeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE")
-}
-
-func GetKubeConfig() string {
-	return filepath.Join(HomeDir(), ".kube", "config")
-}
+var OAMLabel = map[string]string{"app.kubernetes.io/part-of": "kubevela"}
 
 // DoesNamespaceExist check namespace exist
 func DoesNamespaceExist(c client.Client, namespace string) (bool, error) {
@@ -55,7 +25,9 @@ func DoesNamespaceExist(c client.Client, namespace string) (bool, error) {
 }
 
 func NewNamespace(c client.Client, namespace string) error {
-	ns := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+	ns := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace,
+		// marking a special label for promethus monitoring.
+		Labels: OAMLabel}}
 	err := c.Create(context.Background(), ns)
 	if err != nil {
 		return err
