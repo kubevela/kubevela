d74d61dee03e84c9afe14c77f7466fa7689d85d8
Weiping Cai <weiping.cai@daocloud.io>
2020-12-21 10:52:19 +0800

    support mutli storage resources,define storage interface and implement local storage
    Signed-off-by: Weiping Cai <weiping.cai@daocloud.io>

--- pkg/storage/driver/local_test.go
+++ pkg/storage/driver/local_test.go
@@ -1,1 +1,211 @@
 package driver
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/oam-dev/kubevela/pkg/appfile"
+	"github.com/oam-dev/kubevela/pkg/appfile/template"
+)
+
+var dir string
+var tm template.Manager
+var afile *appfile.AppFile
+var appName = "testsvc"
+var envName = "default"
+
+func init() {
+	dir, _ = getApplicationDir(envName)
+	tm, _ = template.Load()
+	afile = appfile.NewAppFile()
+	_ = ioutil.WriteFile(filepath.Join(dir, appName+".yaml"), nil, 0644)
+}
+
+func TestLocal_Delete(t *testing.T) {
+	type args struct {
+		envName string
+		appName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"testLocalDelete1", args{envName: envName, appName: appName}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Local{}
+			if err := l.Delete(tt.args.envName, tt.args.appName); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestLocal_Get(t *testing.T) {
+	type args struct {
+		envName string
+		appName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *RespApplication
+		wantErr bool
+	}{
+		{"TestLocal_Get1", args{envName: envName, appName: appName}, &RespApplication{AppFile: afile, Tm: tm}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Local{}
+			got, err := l.Get(tt.args.envName, tt.args.appName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLocal_List(t *testing.T) {
+	type args struct {
+		envName string
+	}
+	want := make([]*RespApplication, 0)
+	want = append(want, &RespApplication{afile, tm})
+	tests := []struct {
+		name    string
+		args    args
+		want    []*RespApplication
+		wantErr bool
+	}{
+		{"TestLocal_List1", args{envName}, want, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Local{}
+			got, err := l.List(tt.args.envName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("List() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) == 0 {
+				t.Errorf("List() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLocal_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"testDriverName", "Local"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Local{}
+			if got := l.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLocal_Save(t *testing.T) {
+	type args struct {
+		app     *RespApplication
+		envName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"TestLocal_Save1", args{&RespApplication{AppFile: afile, Tm: tm}, envName}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Local{}
+			if err := l.Save(tt.args.app, tt.args.envName); (err != nil) != tt.wantErr {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNewLocalStorage(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Local
+	}{
+		{"testNewLocal", &Local{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewLocalStorage(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewLocalStorage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getApplicationDir(t *testing.T) {
+	type args struct {
+		envName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"testGetApplicationDir", args{envName: envName}, "/envs/default/applications", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getApplicationDir(tt.args.envName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getApplicationDir() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !strings.Contains(got, tt.want) {
+				t.Errorf("getApplicationDir() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_loadFromFile(t *testing.T) {
+	type args struct {
+		fileName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *RespApplication
+		wantErr bool
+	}{
+		{"testRespApp", args{fileName: filepath.Join(dir, appName+".yaml")}, &RespApplication{AppFile: afile, Tm: tm}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := loadFromFile(tt.args.fileName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadFromFile() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -1,1 +1,151 @@
 package storage
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/oam-dev/kubevela/pkg/storage/driver"
+)
+
+func TestNewStorage(t *testing.T) {
+	type args struct {
+		driverName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Storage
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewStorage(tt.args.driverName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewStorage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStorage_Delete(t *testing.T) {
+	type fields struct {
+		Driver driver.Driver
+	}
+	type args struct {
+		envName string
+		appName string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Storage{
+				Driver: tt.fields.Driver,
+			}
+			if err := s.Delete(tt.args.envName, tt.args.appName); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStorage_Get(t *testing.T) {
+	type fields struct {
+		Driver driver.Driver
+	}
+	type args struct {
+		envName string
+		appName string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *driver.RespApplication
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Storage{
+				Driver: tt.fields.Driver,
+			}
+			got, err := s.Get(tt.args.envName, tt.args.appName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStorage_List(t *testing.T) {
+	type fields struct {
+		Driver driver.Driver
+	}
+	type args struct {
+		envName string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []*driver.RespApplication
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Storage{
+				Driver: tt.fields.Driver,
+			}
+			got, err := s.List(tt.args.envName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("List() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("List() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStorage_Save(t *testing.T) {
+	type fields struct {
+		Driver driver.Driver
+	}
+	type args struct {
+		app     *driver.RespApplication
+		envName string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Storage{
+				Driver: tt.fields.Driver,
+			}
+			if err := s.Save(tt.args.app, tt.args.envName); (err != nil) != tt.wantErr {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
