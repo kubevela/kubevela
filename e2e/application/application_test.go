cc590d7c423d89dc73a4eae76accedffc1f2c379
天元 <jianbo.sjb@alibaba-inc.com>
2021-01-05 18:10:22 +0800

    using vela up to deploy

--- e2e/application/application_test.go
+++ e2e/application/application_test.go
@@ -1,20 +1,19 @@
 package e2e
 
 import (
-	"fmt"
-
 	"github.com/onsi/ginkgo"
 
 	"github.com/oam-dev/kubevela/e2e"
 )
 
 var (
-	envName         = "env-application"
-	workloadType    = "webservice"
-	applicationName = "app-basic"
-	traitAlias      = "scaler"
-	appNameForInit  = "initmyapp"
-	jsonAppFile     = `{"name":"nginx-vela","services":{"nginx":{"type":"webservice","image":"nginx:1.9.4","port":80}}}`
+	envName             = "env-application"
+	workloadType        = "webservice"
+	applicationName     = "app-basic"
+	traitAlias          = "scaler"
+	appNameForInit      = "initmyapp"
+	jsonAppFile         = `{"name":"nginx-vela","services":{"nginx":{"type":"webservice","image":"nginx:1.9.4","port":80}}}`
+	appbasicJsonAppFile = `{"name":"app-basic","services":{"app-basic":{"type":"webservice","image":"nginx:1.9.4","port":80}}}`
 )
 
 var _ = ginkgo.Describe("Application", func() {
@@ -23,8 +22,7 @@ var _ = ginkgo.Describe("Application", func() {
 	e2e.DeleteEnvFunc("env delete", envName)
 	e2e.EnvInitContext("env init", envName)
 	e2e.EnvSetContext("env set", envName)
-	e2e.WorkloadRunContext("deploy", fmt.Sprintf("vela svc deploy -t %s %s -p 80 --image nginx:1.9.4",
-		workloadType, applicationName))
+	e2e.JsonAppFileContext("deploy app-basic", appbasicJsonAppFile)
 	e2e.TraitManualScalerAttachContext("vela attach scaler trait", traitAlias, applicationName)
 	e2e.ComponentListContext("ls", applicationName, workloadType, traitAlias)
 	e2e.ApplicationShowContext("show", applicationName, workloadType)
