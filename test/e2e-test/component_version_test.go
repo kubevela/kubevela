99fc04678cbf0df8f2ac78f57440fdf0fa953fac
Ryan Zhang <yangzhangrice@hotmail.com>
2021-02-18 20:03:26 -0800

    lint

--- test/e2e-test/component_version_test.go
+++ test/e2e-test/component_version_test.go
@@ -166,7 +166,9 @@ var _ = Describe("Versioning mechanism of components", func() {
 			revisionNameV1 := cmpV1.Status.LatestRevision.Name
 			By("Get corresponding ControllerRevision of Component v1")
 			cr := &appsv1.ControllerRevision{}
-			Expect(k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: revisionNameV1}, cr)).Should(Succeed())
+			Eventually(
+				k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: revisionNameV1}, cr),
+				time.Second*15, time.Millisecond*500).ShouldNot(HaveOccurred())
 			By("Check revision seq number")
 			Expect(cr.Revision).Should(Equal(int64(1)))
 
