15ffac9a4621fe7ed77d47d73db6f1af83f6edde
天元 <jianbo.sjb@alibaba-inc.com>
2021-03-20 17:11:33 +0800

    migrate dashboard to velacp(https://github.com/oam-dev/velacp/pull/73)

--- references/dashboard/src/components/Footer/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import { DefaultFooter } from '@ant-design/pro-layout';
-
-export default () => <DefaultFooter copyright="2020 All Rights Reserved by KubeVela" links={[]} />;
--- references/dashboard/src/pages/Application/Create/components/FormGroup/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { Card } from 'antd';
-
-const FormGroup: React.FC<{ title: React.ReactNode }> = ({ title, children }) => {
-  return <Card title={title}>{children}</Card>;
-};
-export default FormGroup;
