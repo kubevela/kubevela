3cb9fa79bf93c673adff38dba3a031e2bda351e8
yangsoon <yangsoonlx@gmail.com>
2021-03-31 16:01:53 +0800

    fix index.js (#1380)

--- docs/index.js
+++ docs/index.js
@@ -93,13 +93,17 @@ const WhatIs = () => (
         <div className="col col--6">
           <h1><Translate>What is KubeVela?</Translate></h1>
           <p className="hero__subtitle">
-            <small>
-              <strong><Translate>For platform builders</Translate></strong>: <Translate>
-                KubeVela serves as a framework that empowers them to create user friendly yet highly extensible platforms at ease
+              <small>
+                  <strong><Translate>For platform builders</Translate></strong>: <Translate>
+                  KubeVela is a platform engine that empowers you to create developer-centric experience in robust, repeatable
+                  and extensible approach. Think about a plug-in that turns your Kubernetes cluster into a </Translate>
+                  <i>Heroku</i> <Translate> via
+                  abstractions designed by yourself.
               </Translate>
-              <br />
-              <strong><Translate>For developers</Translate></strong>: <Translate>
-                such Application abstraction built with KubeVela will enable them to design and ship their applications to Kubernetes with minimal effort.
+                  <br />
+                  <br />
+                  <strong><Translate>For developers</Translate></strong>: <Translate>
+                  enjoy the abstractions built by platform team and manage applications with minimal effort.
               </Translate>
               </small>
           </p>
