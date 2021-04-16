34684650c3ee7edb714b5c63537e04f8a6c53fa3
xiqiu618 <73998049+xiqiu618@users.noreply.github.com>
2021-04-16 13:05:09 +0800

    fix appContext update failed error (#1500)
    * fix appContext update failed error
    
    * Update pkg/controller/core.oam.dev/v1alpha2/applicationcontext/applicationcontext_controller.go
    
    Co-authored-by: Jianbo Sun <wonderflow.sun@gmail.com>
    
    * gofmt comments
    
    Co-authored-by: Jianbo Sun <wonderflow.sun@gmail.com>

--- pkg/controller/core.oam.dev/v1alpha2/applicationcontext/applicationcontext_controller.go
+++ pkg/controller/core.oam.dev/v1alpha2/applicationcontext/applicationcontext_controller.go
@@ -107,9 +107,11 @@ func (r *Reconciler) Reconcile(request reconcile.Request) (reconcile.Result, err
 	// call into the old ac Reconciler and copy the status back
 	acReconciler := ac.NewReconciler(r.mgr, dm, r.log, ac.WithRecorder(r.record), ac.WithApplyOnceOnlyMode(r.applyMode))
 	reconResult := acReconciler.ACReconcile(ctx, appConfig, r.log)
+	appContextPatch := client.MergeFrom(appContext.DeepCopy())
 	appContext.Status = appConfig.Status
 	// always update ac status and set the error
-	err = errors.Wrap(r.client.Status().Update(ctx, appContext), errUpdateAppContextStatus)
+	// this should be the only place for status of AppContext to update, so we can patch to avoid update conflicts caused by `resourceVersion` changed by spec.
+	err = errors.Wrap(r.client.Status().Patch(ctx, appContext, appContextPatch), errUpdateAppContextStatus)
 	// use the controller build-in backoff mechanism if an error occurs
 	if err != nil {
 		reconResult.RequeueAfter = 0
