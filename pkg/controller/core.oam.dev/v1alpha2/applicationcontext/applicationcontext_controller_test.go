035dc384f2464b3fc034a912850576b1c4b997f6
Zheng Xi Zhou <zzxwill@gmail.com>
2021-04-30 11:49:18 +0800

    Add unittests for ApplicationContext

---
+++ pkg/controller/core.oam.dev/v1alpha2/applicationcontext/applicationcontext_controller_test.go
@@ -0,0 +1,167 @@
+/*
+ Copyright 2021 The KubeVela Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+*/
+
+package applicationcontext
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1alpha2"
+	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
+	"github.com/oam-dev/kubevela/pkg/oam/util"
+)
+
+var _ = Describe("Test ApplicationContext Controller", func() {
+	ctx := context.Background()
+
+	It("Applying ApplicationContext", func() {
+		Context("appContext doesn't exist", func() {
+			By("apply an ApplicationContext")
+			var (
+				appContextName  = "app1"
+				appRevisionName = "xxx-v1"
+				componentName   = "comp1"
+				ns              = "default"
+				appContext      = v1alpha2.ApplicationContext{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "core.oam.dev/v1alpha2",
+						Kind:       "ApplicationContext",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      appContextName,
+						Namespace: ns,
+					},
+					Spec: v1alpha2.ApplicationContextSpec{
+						ApplicationRevisionName: appRevisionName,
+					},
+				}
+				req = reconcile.Request{NamespacedName: client.ObjectKey{Name: appContextName, Namespace: ns}}
+			)
+			Expect(k8sClient.Create(ctx, &appContext)).Should(Succeed())
+			reconcileRetry(&r, req)
+
+			By("check the ApplicationContext")
+			var got v1alpha2.ApplicationContext
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Namespace: ns, Name: appContextName}, &got)).Should(BeNil())
+			reconcileRetry(&r, req)
+
+			By("create an component")
+			workload := v1.Deployment{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Deployment",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      componentName,
+					Namespace: ns,
+				},
+				Spec: v1.DeploymentSpec{
+					Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app.oam.dev/component": componentName}},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app.oam.dev/component": componentName}},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{{Image: "nginx", Name: componentName}}},
+					},
+				},
+			}
+
+			comp := v1alpha2.Component{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Component",
+					APIVersion: "core.oam.dev/v1alpha2",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      componentName,
+					Namespace: ns,
+				},
+				Spec: v1alpha2.ComponentSpec{
+					Workload: util.Object2RawExtension(workload),
+				},
+			}
+			Expect(k8sClient.Create(ctx, &comp)).Should(Succeed())
+
+			By("create an application revision")
+			app := v1beta1.Application{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Application",
+					APIVersion: "core.oam.dev/v1beta1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      appContextName,
+					Namespace: ns,
+				},
+				Spec: v1beta1.ApplicationSpec{
+					Components: []v1beta1.ApplicationComponent{
+						{
+							Name:       componentName,
+							Type:       "worker",
+							Properties: runtime.RawExtension{Raw: []byte(`{"cmd":["sleep","1000"],"image":"busybox","config":"myconfig"}`)},
+						},
+					},
+				},
+			}
+
+			appConfig := v1alpha2.ApplicationConfiguration{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.oam.dev/v1alpha2",
+					Kind:       "ApplicationConfiguration",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      appContextName,
+					Namespace: ns,
+				},
+				Spec: v1alpha2.ApplicationConfigurationSpec{
+					Components: []v1alpha2.ApplicationConfigurationComponent{{ComponentName: "comp1"}}},
+			}
+
+			appRevision := v1beta1.ApplicationRevision{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      appRevisionName,
+					Namespace: ns,
+				},
+				Spec: v1beta1.ApplicationRevisionSpec{
+					Application:              app,
+					ApplicationConfiguration: util.Object2RawExtension(appConfig),
+				},
+			}
+			Expect(k8sClient.Create(ctx, &appRevision)).Should(Succeed())
+			Eventually(func() int {
+				By("Reconcile")
+				reconcileRetry(&r, req)
+				if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: ns, Name: appRevisionName}, &v1beta1.ApplicationRevision{}); err != nil {
+					return 0
+				}
+				return 1
+			}, 5*time.Second, time.Second).Should(Equal(1))
+
+			By("check application context")
+			var ac v1alpha2.ApplicationContext
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Namespace: ns, Name: appContextName}, &ac)).Should(BeNil())
+			Expect(string(ac.Status.ConditionedStatus.Conditions[0].Status)).Should(Equal("False"))
+		})
+	})
+})
---
+++ pkg/controller/core.oam.dev/v1alpha2/applicationcontext/suit_test.go
@@ -0,0 +1,131 @@
+/*
+
+ Copyright 2021 The KubeVela Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+*/
+
+package applicationcontext
+
+import (
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/crossplane/crossplane-runtime/pkg/event"
+	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	crdv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	oamCore "github.com/oam-dev/kubevela/apis/core.oam.dev"
+	core "github.com/oam-dev/kubevela/pkg/controller/core.oam.dev"
+	ac "github.com/oam-dev/kubevela/pkg/controller/core.oam.dev/v1alpha2/applicationconfiguration"
+	"github.com/oam-dev/kubevela/pkg/oam/discoverymapper"
+)
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+var controllerDone chan struct{}
+var r Reconciler
+var defRevisionLimit = 5
+
+func TestComponentDefinition(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ApplicationContext Suite")
+}
+
+var _ = BeforeSuite(func(done Done) {
+	By("Bootstrapping test environment")
+	useExistCluster := false
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("../../../../..", "charts/vela-core/crds"), // this has all the required CRDs,
+		},
+		UseExistingCluster: &useExistCluster,
+	}
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	err = oamCore.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(crdv1.AddToScheme(scheme.Scheme)).Should(BeNil())
+
+	By("Create the k8s client")
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+
+	By("Starting the controller in the background")
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:             scheme.Scheme,
+		MetricsBindAddress: "0",
+		Port:               48082,
+	})
+	Expect(err).ToNot(HaveOccurred())
+	dm, err := discoverymapper.New(mgr.GetConfig())
+	Expect(err).ToNot(HaveOccurred())
+	_, err = dm.Refresh()
+	Expect(err).ToNot(HaveOccurred())
+
+	var name = "ApplicationContext"
+
+	logr := ctrl.Log.WithName("ApplicationContext")
+	r = Reconciler{
+		client:    mgr.GetClient(),
+		log:       logging.NewLogrLogger(logr).WithValues("suitTest", name),
+		mgr:       mgr,
+		record:    event.NewAPIRecorder(mgr.GetEventRecorderFor(name)),
+		applyMode: core.ApplyOnceOnlyOff,
+	}
+	compHandler := &ac.ComponentHandler{
+		Client:                mgr.GetClient(),
+		Logger:                logging.NewLogrLogger(logr),
+		RevisionLimit:         defRevisionLimit,
+		CustomRevisionHookURL: "",
+	}
+	Expect(r.SetupWithManager(mgr, compHandler)).ToNot(HaveOccurred())
+	controllerDone = make(chan struct{}, 1)
+	go func() {
+		defer GinkgoRecover()
+		Expect(mgr.Start(controllerDone)).ToNot(HaveOccurred())
+	}()
+
+	close(done)
+}, 60)
+
+var _ = AfterSuite(func() {
+	By("Stop the controller")
+	close(controllerDone)
+
+	By("Tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
+
+func reconcileRetry(r reconcile.Reconciler, req reconcile.Request) {
+	Eventually(func() error {
+		_, err := r.Reconcile(req)
+		return err
+	}, 15*time.Second, time.Second).Should(BeNil())
+}
