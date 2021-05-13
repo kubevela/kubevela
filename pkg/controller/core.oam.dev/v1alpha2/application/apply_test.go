52a19e23aad47dcd48cc25a2f2e7a30f09a9adb5
Zheng Xi Zhou <zzxwill@gmail.com>
2021-05-13 17:46:31 +0800

    add unit tests

--- pkg/controller/core.oam.dev/v1alpha2/application/apply_test.go
+++ pkg/controller/core.oam.dev/v1alpha2/application/apply_test.go
@@ -24,17 +24,22 @@ import (
 
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
 	"github.com/ghodss/yaml"
+	"github.com/oam-dev/kubevela/pkg/appfile"
+	terraformtypes "github.com/oam-dev/terraform-controller/api/types"
+	terraformapi "github.com/oam-dev/terraform-controller/api/v1beta1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/oam-dev/kubevela/apis/core.oam.dev/common"
 	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1alpha2"
 	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
+	velatypes "github.com/oam-dev/kubevela/apis/types"
 	"github.com/oam-dev/kubevela/pkg/controller/utils"
 	"github.com/oam-dev/kubevela/pkg/oam/util"
 	"github.com/oam-dev/kubevela/pkg/utils/apply"
@@ -280,5 +285,59 @@ var _ = Describe("Test applyHelmModuleResources", func() {
 		}}}
 		err := handler.applyHelmModuleResources(ctx, comp, nil)
 		Expect(err).Should(HaveOccurred())
+	})
+})
+
+var _ = Describe("Test statusAggregate", func() {
+	It("the component is Terraform type", func() {
+		var (
+			ctx           = context.TODO()
+			componentName = "sample-oss"
+			ns            = "default"
+			h             = &appHandler{r: reconciler, app: &v1beta1.Application{
+				TypeMeta:   metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{Namespace: ns},
+			}}
+			appFile = &appfile.Appfile{
+				Workloads: []*appfile.Workload{
+					{
+						Name:               componentName,
+						FullTemplate:       &appfile.Template{Reference: common.WorkloadGVK{APIVersion: "v1", Kind: "A1"}},
+						CapabilityCategory: velatypes.TerraformCategory,
+					},
+				},
+			}
+		)
+
+		By("aggregate status")
+		statuses, healthy, err := h.statusAggregate(appFile)
+		Expect(statuses).Should(BeNil())
+		Expect(healthy).Should(Equal(false))
+		Expect(err).Should(HaveOccurred())
+
+		By("create Terraform configuration")
+		configuration := terraformapi.Configuration{
+			TypeMeta:   metav1.TypeMeta{APIVersion: "terraform.core.oam.dev/v1beta1", Kind: "Configuration"},
+			ObjectMeta: metav1.ObjectMeta{Name: componentName, Namespace: ns},
+		}
+		k8sClient.Create(ctx, &configuration)
+
+		By("aggregate status again")
+		statuses, healthy, err = h.statusAggregate(appFile)
+		Expect(len(statuses)).Should(Equal(1))
+		Expect(healthy).Should(Equal(false))
+		Expect(err).Should(BeNil())
+
+		By("set status for Terraform configuration")
+		var gotConfiguration terraformapi.Configuration
+		k8sClient.Get(ctx, client.ObjectKey{Namespace: ns, Name: componentName}, &gotConfiguration)
+		gotConfiguration.Status.State = terraformtypes.Available
+		k8sClient.Status().Update(ctx, &gotConfiguration)
+
+		By("aggregate status one more time")
+		statuses, healthy, err = h.statusAggregate(appFile)
+		Expect(len(statuses)).Should(Equal(1))
+		Expect(healthy).Should(Equal(true))
+		Expect(err).Should(BeNil())
 	})
 })
