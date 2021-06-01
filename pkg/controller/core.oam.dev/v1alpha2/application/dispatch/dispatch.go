018c0e8df011247c9200c1fbd684d474f42c24c8
roy wang <seiwy2010@gmail.com>
2021-06-01 18:37:58 +0900

    implement dispatch pkg
    add unit test
    
    add log
    
    Signed-off-by: roy wang <seiwy2010@gmail.com>

---
+++ pkg/controller/core.oam.dev/v1alpha2/application/dispatch/dispatch.go
@@ -0,0 +1,265 @@
+/*
+Copyright 2021 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dispatch
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/pkg/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
+	"github.com/oam-dev/kubevela/pkg/utils/apply"
+)
+
+// NewAppManifestsDispatcher creates an AppManifestsDispatcher.
+func NewAppManifestsDispatcher(c client.Client, appRev *v1beta1.ApplicationRevision) *AppManifestsDispatcher {
+	return &AppManifestsDispatcher{
+		c:          c,
+		applicator: apply.NewAPIApplicator(c),
+		appRev:     appRev,
+		gcHandler:  NewGCHandler(c, appRev.Namespace),
+	}
+}
+
+// AppManifestsDispatcher dispatch application manifests into K8s and record the dispatched manifests' references in a
+// resource tracker which is named by a particular rule: name = appRevision's Name + appRevision's namespace.
+// A bundle of manifests to be dispatched MUST come from the given application revision.
+type AppManifestsDispatcher struct {
+	c          client.Client
+	applicator apply.Applicator
+	gcHandler  GarbageCollector
+
+	appRev *v1beta1.ApplicationRevision
+	oldRT  *v1beta1.ResourceTracker
+	skipGC bool
+
+	appRevName string
+	namespace  string
+	newRTName  string
+	newRT      *v1beta1.ResourceTracker
+}
+
+// EnableGC return an AppManifestsDispatcher that always do GC after dispatching resources.
+// GC will calculate diff between the dispatched resouces and ones recorded in the given resource tracker.
+func (a *AppManifestsDispatcher) EnableGC(rt *v1beta1.ResourceTracker) *AppManifestsDispatcher {
+	if rt != nil {
+		a.oldRT = rt.DeepCopy()
+	}
+	return a
+}
+
+// EnableUpgradeAndSkipGC return an AppManifestsDispatcher that skips GC after dispatching resources.
+// For the unchanged resources, dispatcher will update their owner to the newly created resource tracker.
+// It's helpful in a rollout scenario where new revision is going to create a new workload while the old one should not
+// be deleted before rollout is terminated.
+func (a *AppManifestsDispatcher) EnableUpgradeAndSkipGC(rt *v1beta1.ResourceTracker) *AppManifestsDispatcher {
+	if rt != nil {
+		a.oldRT = rt.DeepCopy()
+		a.skipGC = true
+	}
+	return a
+}
+
+// Dispatch apply manifests into k8s and return a resource tracker recording applied manifests' references.
+// If GC is enabled, it will do GC after applying.
+// If 'UpgradeAndSkipGC' is enabled, it will:
+// - create new resources if not exist before
+// - update unchanged resources' owner from the old resource tracker to the new one
+// - skip deleting(GC) any resources
+func (a *AppManifestsDispatcher) Dispatch(ctx context.Context, manifests []*unstructured.Unstructured) (*v1beta1.ResourceTracker, error) {
+	if err := a.validateAndComplete(ctx); err != nil {
+		return nil, err
+	}
+	if err := a.createOrGetResourceTracker(ctx); err != nil {
+		return nil, err
+	}
+	if err := a.applyAndRecordManifests(ctx, manifests); err != nil {
+		return nil, err
+	}
+	if !a.skipGC && a.oldRT != nil && a.oldRT.Name != a.newRTName {
+		if err := a.gcHandler.GarbageCollect(ctx, a.oldRT, a.newRT); err != nil {
+			return nil, errors.WithMessagef(err, "cannot do GC based on resource trackers %q and %q", a.oldRT.Name, a.newRTName)
+		}
+	}
+	return a.newRT.DeepCopy(), nil
+}
+
+// ReferenceScopes add workload reference to scopes' workloadRefPath
+func (a *AppManifestsDispatcher) ReferenceScopes(ctx context.Context, wlRef *v1beta1.TypedReference, scopes []*v1beta1.TypedReference) error {
+	// TODO handle scopes
+	return nil
+}
+
+// DereferenceScopes remove workload reference from scopes' workloadRefPath
+func (a *AppManifestsDispatcher) DereferenceScopes(ctx context.Context, wlRef *v1beta1.TypedReference, scopes []*v1beta1.TypedReference) error {
+	// TODO handle scopes
+	return nil
+}
+
+func (a *AppManifestsDispatcher) validateAndComplete(ctx context.Context) error {
+	if a.appRev == nil {
+		return errors.New("given application revision is nil")
+	}
+	if a.appRev.Name == "" || a.appRev.Namespace == "" {
+		return errors.New("given application revision has no name or namespace")
+	}
+	a.appRevName = a.appRev.Name
+	a.namespace = a.appRev.Namespace
+	a.newRTName = ConstructResourceTrackerName(a.appRevName, a.namespace)
+
+	// no matter GC or UpgradeAndSkipGC, it requires a valid and existing resource tracker
+	if a.oldRT != nil {
+		existingOldRT := &v1beta1.ResourceTracker{}
+		if err := a.c.Get(ctx, client.ObjectKey{Name: a.oldRT.Name}, existingOldRT); err != nil {
+			return errors.Errorf("given resource tracker %q doesn't exist", a.oldRT.Name)
+		}
+		a.oldRT = existingOldRT
+	}
+	klog.InfoS("Given old resource tracker is nil, so skip GC", "appRevision", klog.KObj(a.appRev))
+	return nil
+}
+
+func (a *AppManifestsDispatcher) createOrGetResourceTracker(ctx context.Context) error {
+	rt := &v1beta1.ResourceTracker{}
+	err := a.c.Get(ctx, client.ObjectKey{Name: a.newRTName}, rt)
+	if err == nil {
+		klog.InfoS("Found a resource tracker matching current app revision", "resourceTracker", rt.Name)
+		// already exists, no need to update
+		// because we assume the manifests' references from a specific application revision never change
+		a.newRT = rt
+		return nil
+	}
+	if !kerrors.IsNotFound(err) {
+		return errors.Wrap(err, "cannot get resource tracker")
+	}
+	klog.InfoS("Going to create a resource tracker", "resourceTracker", a.newRTName)
+	rt.SetName(a.newRTName)
+	// use AppRevision as the controller owner of resource tracker
+	ctrlOwnerRef := metav1.OwnerReference{
+		APIVersion: a.appRev.APIVersion,
+		Kind:       a.appRev.Kind,
+		Name:       a.appRev.Name,
+		UID:        a.appRev.UID,
+		Controller: pointer.BoolPtr(true),
+	}
+	rt.SetOwnerReferences([]metav1.OwnerReference{ctrlOwnerRef})
+	if err := a.c.Create(ctx, rt); err != nil {
+		klog.ErrorS(err, "Failed to create a resource tracker", "resourceTracker", a.newRTName)
+		return errors.Wrap(err, "cannot create resource tracker")
+	}
+	a.newRT = rt
+	return nil
+}
+
+func (a *AppManifestsDispatcher) applyAndRecordManifests(ctx context.Context, manifests []*unstructured.Unstructured) error {
+	applyOpts := []apply.ApplyOption{}
+	if a.oldRT != nil && a.oldRT.Name != a.newRTName {
+		klog.InfoS("Going to apply and upgrade resources", "from", a.oldRT.Name, "to", a.newRTName)
+		// if two RT's names are different, it means dispatching operation happens in an upgrade or rollout scenario
+		// in such two scenarios, for those unchanged manifests, we will
+		// - check existing resources are controlled by the old resource tracker
+		// - set new resource tracker as their controller owner
+		applyOpts = append(applyOpts, apply.MustBeControllableBy(a.oldRT.UID))
+	} else {
+		applyOpts = append(applyOpts, apply.MustBeControllableBy(a.newRT.UID))
+	}
+
+	ownerRef := metav1.OwnerReference{
+		APIVersion:         v1beta1.SchemeGroupVersion.String(),
+		Kind:               reflect.TypeOf(v1beta1.ResourceTracker{}).Name(),
+		Name:               a.newRT.Name,
+		UID:                a.newRT.UID,
+		Controller:         pointer.BoolPtr(true),
+		BlockOwnerDeletion: pointer.BoolPtr(true),
+	}
+	for _, rsc := range manifests {
+		// each resource applied by dispatcher MUST be controlled by resource tracker
+		setOrOverrideControllerOwner(rsc, ownerRef)
+		if err := a.applicator.Apply(ctx, rsc, applyOpts...); err != nil {
+			klog.ErrorS(err, "Failed to apply a resource", "object",
+				klog.KObj(rsc), "apiVersion", rsc.GetAPIVersion(), "kind", rsc.GetKind())
+			return errors.Wrapf(err, "cannot apply manifest, name: %q apiVersion: %q kind: %q",
+				rsc.GetName(), rsc.GetAPIVersion(), rsc.GetKind())
+		}
+		klog.InfoS("Successfully apply a resource", "object",
+			klog.KObj(rsc), "apiVersion", rsc.GetAPIVersion(), "kind", rsc.GetKind())
+	}
+	return a.updateResourceTrackerStatus(ctx, manifests)
+}
+
+func (a *AppManifestsDispatcher) updateResourceTrackerStatus(ctx context.Context, appliedManifests []*unstructured.Unstructured) error {
+	// merge applied resources and already recorded ones
+	trackedResources := []v1beta1.TypedReference{}
+	for _, rsc := range appliedManifests {
+		ref := v1beta1.TypedReference{
+			APIVersion: rsc.GetAPIVersion(),
+			Kind:       rsc.GetKind(),
+			Name:       rsc.GetName(),
+			Namespace:  rsc.GetNamespace(),
+		}
+		alreadyTracked := false
+		for _, existing := range a.newRT.Status.TrackedResources {
+			if existing.APIVersion == ref.APIVersion && existing.Kind == ref.Kind &&
+				existing.Name == ref.Name && existing.Namespace == ref.Namespace {
+				alreadyTracked = true
+				break
+			}
+		}
+		if alreadyTracked {
+			continue
+		}
+		trackedResources = append(trackedResources, ref)
+	}
+	a.newRT.Status.TrackedResources = trackedResources
+
+	// TODO move TrackedResources from status to spec
+	// update status with retry
+	copyRT := a.newRT.DeepCopy()
+	sts := copyRT.Status
+	if err := retry.RetryOnConflict(retry.DefaultBackoff, func() (err error) {
+		if err = a.c.Get(ctx, client.ObjectKey{Name: a.newRTName}, copyRT); err != nil {
+			return
+		}
+		copyRT.Status = sts
+		return a.c.Status().Update(ctx, copyRT)
+	}); err != nil {
+		klog.ErrorS(err, "Failed to update resource tracker status", "resourceTracker", a.newRTName)
+		return errors.Wrap(err, "cannot update resource tracker status")
+	}
+	klog.InfoS("Successfully update resource tracker status", "resourceTracker", a.newRTName)
+	return nil
+}
+
+func setOrOverrideControllerOwner(obj *unstructured.Unstructured, controllerOwner metav1.OwnerReference) {
+	ownerRefs := []metav1.OwnerReference{controllerOwner}
+	for _, owner := range obj.GetOwnerReferences() {
+		if owner.Controller != nil && *owner.Controller &&
+			owner.UID != controllerOwner.UID {
+			owner.Controller = pointer.BoolPtr(false)
+		}
+		ownerRefs = append(ownerRefs, owner)
+	}
+	obj.SetOwnerReferences(ownerRefs)
+}
---
+++ pkg/controller/core.oam.dev/v1alpha2/application/dispatch/dispatch_suite_test.go
@@ -0,0 +1,342 @@
+/*
+Copyright 2021 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dispatch
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/utils/pointer"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
+	"github.com/oam-dev/kubevela/pkg/oam/util"
+)
+
+var (
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+)
+
+func TestDispatch(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Dispatch Suite")
+}
+
+var _ = BeforeSuite(func(done Done) {
+	rand.Seed(time.Now().UnixNano())
+	By("bootstrapping test environment")
+	var yamlPath string
+	if _, set := os.LookupEnv("COMPATIBILITY_TEST"); set {
+		yamlPath = "../../../../../../test/compatibility-test/testdata"
+	} else {
+		yamlPath = filepath.Join("../../../../../..", "charts", "vela-core", "crds")
+	}
+	testEnv = &envtest.Environment{
+		UseExistingCluster: pointer.BoolPtr(false),
+		CRDDirectoryPaths:  []string{yamlPath},
+	}
+
+	cfg, err := testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	var testScheme = runtime.NewScheme()
+	err = v1beta1.SchemeBuilder.AddToScheme(testScheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = scheme.AddToScheme(testScheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: testScheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+	close(done)
+}, 30)
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = Describe("Test AppManifestsDispatcher", func() {
+	var (
+		ctx                                    = context.Background()
+		namespace                              string
+		deploy1, deploy2, svc1, svc2, pv1, pv2 *unstructured.Unstructured
+		appRev1, appRev2                       *v1beta1.ApplicationRevision
+	)
+
+	var (
+		deployName1 = "deploy-1"
+		deployName2 = "deploy-2"
+		svcName1    = "svc-1"
+		svcName2    = "svc-2"
+		// persistent volume is cluster-scoped, generate random name for each case to avoid interference
+		pvName1, pvName2 string
+		appRevName1      = "app-v1"
+		appRevName2      = "app-v2"
+	)
+
+	BeforeEach(func() {
+		By("Create test namespace")
+		namespace = fmt.Sprintf("%s-%s", "dispatch-test", strconv.FormatInt(rand.Int63(), 16))
+		Expect(k8sClient.Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}})).Should(Succeed())
+
+		By("Init test data")
+		var (
+			d1, d2 *appsv1.Deployment       // represent common workload
+			s1, s2 *corev1.Service          // represent common trait
+			p1, p2 *corev1.PersistentVolume // represent cluster-scoped resource
+		)
+		d := &appsv1.Deployment{TypeMeta: metav1.TypeMeta{APIVersion: "apps/v1", Kind: "Deployment"}}
+		d.SetNamespace(namespace)
+		d.Spec.Selector = &metav1.LabelSelector{MatchLabels: map[string]string{"app": "nginx"}}
+		d.Spec.Template = corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app": "nginx"}},
+			Spec: corev1.PodSpec{Containers: []corev1.Container{{
+				Name:  "nginx",
+				Image: "nginx: 1.14.2",
+				Ports: []corev1.ContainerPort{{Name: "nginx", ContainerPort: int32(8080)}}}}},
+		}
+		d1 = d.DeepCopy()
+		d1.SetName(deployName1)
+		deploy1, _ = util.Object2Unstructured(d1)
+		d2 = d.DeepCopy()
+		d2.SetName(deployName2)
+		deploy2, _ = util.Object2Unstructured(d2)
+
+		svc := &corev1.Service{
+			TypeMeta: metav1.TypeMeta{APIVersion: "v1", Kind: "Service"},
+			Spec:     corev1.ServiceSpec{Ports: []corev1.ServicePort{{Port: int32(8080)}}},
+		}
+		svc.SetNamespace(namespace)
+		s1 = svc.DeepCopy()
+		s1.SetName(svcName1)
+		svc1, _ = util.Object2Unstructured(s1)
+		s2 = svc.DeepCopy()
+		s2.SetName(svcName2)
+		svc2, _ = util.Object2Unstructured(s2)
+
+		pv := &corev1.PersistentVolume{
+			TypeMeta: metav1.TypeMeta{APIVersion: "v1", Kind: "PersistentVolume"},
+			Spec: corev1.PersistentVolumeSpec{
+				Capacity: corev1.ResourceList{"storage": resource.MustParse("10Gi")},
+				PersistentVolumeSource: corev1.PersistentVolumeSource{
+					HostPath: &corev1.HostPathVolumeSource{Path: "/mnt/data"},
+				},
+				AccessModes:      []corev1.PersistentVolumeAccessMode{"ReadWriteOnce"},
+				StorageClassName: "manual",
+			},
+		}
+		pvName1 = "pv-1-" + namespace
+		pvName2 = "pv-2-" + namespace
+		p1 = pv.DeepCopy()
+		p1.SetName(pvName1)
+		pv1, _ = util.Object2Unstructured(p1)
+		p2 = pv.DeepCopy()
+		p2.SetName(pvName2)
+		pv2, _ = util.Object2Unstructured(p2)
+
+		appRev := &v1beta1.ApplicationRevision{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "core.oam.dev",
+				Kind:       "ApplicationRevision",
+			},
+		}
+		appRev.SetNamespace(namespace)
+		appRev1 = appRev.DeepCopy()
+		appRev1.SetName(appRevName1)
+		appRev1.SetUID("fake-uid-app-revision-1")
+		appRev2 = appRev.DeepCopy()
+		appRev2.SetName(appRevName2)
+		appRev2.SetUID("fake-uid-app-revision-2")
+	})
+
+	AfterEach(func() {
+		Expect(k8sClient.DeleteAllOf(ctx, &corev1.PersistentVolume{})).Should(Succeed())
+		Expect(k8sClient.Delete(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}})).Should(Succeed())
+	})
+
+	When("Dispatch without upgrading", func() {
+		// real scenario case: release v1
+		It("Test dispatch manifests", func() {
+			By("Dispatch application revision 1")
+			dp := NewAppManifestsDispatcher(k8sClient, appRev1)
+			rt, err := dp.Dispatch(ctx, []*unstructured.Unstructured{deploy1, svc1, pv1})
+			Expect(err).Should(BeNil())
+			By("Verify resources are applied successfully")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, &corev1.PersistentVolume{})).Should(Succeed())
+			getPV1 := &corev1.PersistentVolume{}
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, getPV1)).Should(Succeed())
+
+			By("Verify resource tracker's name")
+			wantName := ConstructResourceTrackerName(appRevName1, namespace)
+			Expect(rt.Name).Should(Equal(wantName))
+
+			By("Verify resource tracker records all applied resources")
+			recordedNames := []string{}
+			for _, r := range rt.Status.TrackedResources {
+				recordedNames = append(recordedNames, r.Name)
+			}
+			Expect(recordedNames).Should(ContainElements(deployName1, svcName1, pvName1))
+		})
+	})
+
+	When("Dispatch for upgrading", func() {
+		// real scenario case: reconcile v1 => v1
+		It("Test use resource tracker from the same revision as the one being dispatched", func() {
+			By("Dispatch application revision 1")
+			dp := NewAppManifestsDispatcher(k8sClient, appRev1)
+			rtForAppV1, err := dp.Dispatch(ctx, []*unstructured.Unstructured{deploy1, svc1, pv1})
+			Expect(err).Should(BeNil())
+			By("Verify resources are applied successfully")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, &corev1.PersistentVolume{})).Should(Succeed())
+
+			By("Dispatch application revision 1 again with v1 as latest RT")
+			dp = NewAppManifestsDispatcher(k8sClient, appRev1).EnableGC(rtForAppV1)
+			_, err = dp.Dispatch(ctx, []*unstructured.Unstructured{deploy1, svc1, pv1})
+			Expect(err).Should(BeNil())
+			By("Verify resources still exist")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, &corev1.PersistentVolume{})).Should(Succeed())
+		})
+
+		It("Test upgrade and garbage collect", func() {
+			// real scenario case: upgrade v1 => v2 and GC
+			By("Dispatch application revision 1")
+			dp := NewAppManifestsDispatcher(k8sClient, appRev1)
+			rtForAppV1, err := dp.Dispatch(ctx, []*unstructured.Unstructured{deploy1, svc1, pv1})
+			Expect(err).Should(BeNil())
+			By("Verify resources are applied successfully")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, &corev1.PersistentVolume{})).Should(Succeed())
+
+			By("Dispatch application revision 2 with v1 as latest RT")
+			dp2 := NewAppManifestsDispatcher(k8sClient, appRev2).EnableGC(rtForAppV1)
+			_, err = dp2.Dispatch(ctx, []*unstructured.Unstructured{deploy2, svc2, pv2})
+			Expect(err).Should(BeNil())
+			By("Verify v2 resources are applied successfully")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName2, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName2, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName2}, &corev1.PersistentVolume{})).Should(Succeed())
+
+			By("Verify v1 resources are deleted (GC works)")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(util.NotFoundMatcher{})
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(util.NotFoundMatcher{})
+			getPV1 := &corev1.PersistentVolume{}
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, getPV1)).Should(Succeed())
+			Expect(persistentVolumeIsDeleted(getPV1)).Should(BeTrue())
+
+			By("Verify v1 resource tracker is deleted (GC works)")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: ConstructResourceTrackerName(appRevName1, namespace)},
+				&v1beta1.ResourceTracker{})).Should(util.NotFoundMatcher{})
+		})
+
+		It("Test upgrade: v2 has the same resources as v1", func() {
+			// real scenario case: upgrade v1 => v2 and GC
+			// and v2 has same resouces as v1, e.g., a trait
+			By("Dispatch application revision 1")
+			dp := NewAppManifestsDispatcher(k8sClient, appRev1)
+			rtForAppV1, err := dp.Dispatch(ctx, []*unstructured.Unstructured{deploy1, svc1, pv1})
+			Expect(err).Should(BeNil())
+			getSvc1 := &corev1.Service{}
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, getSvc1)).Should(Succeed())
+			By("Verify serivce1's owner is resource tracker 1")
+			owner := metav1.GetControllerOf(getSvc1)
+			Expect(owner.Name).Should(Equal(rtForAppV1.Name))
+
+			By("Dispatch application revision 2 with v1 as latest RT")
+			dp2 := NewAppManifestsDispatcher(k8sClient, appRev2).EnableGC(rtForAppV1)
+			rtForAppV2, err := dp2.Dispatch(ctx, []*unstructured.Unstructured{deploy2, svc1, pv2}) // manifests have 'svc1'
+			Expect(err).Should(BeNil())
+
+			By("Verify v1 resources, expect svc1, are deleted (GC works)")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(util.NotFoundMatcher{})
+			getPV1 := &corev1.PersistentVolume{}
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, getPV1)).Should(Succeed())
+			Expect(persistentVolumeIsDeleted(getPV1)).Should(BeTrue())
+
+			By("Verify svc1's owner has beend updated to resource tracker 2")
+			getSvc1 = &corev1.Service{}
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, getSvc1)).Should(Succeed())
+			owner = metav1.GetControllerOf(getSvc1)
+			Expect(owner.Name).Should(Equal(rtForAppV2.Name))
+		})
+
+		It("Test upgrade but skip garbage collect", func() {
+			// real scenario case: template v2 while rollout v1 => v2
+			By("Dispatch application revision 1")
+			dp := NewAppManifestsDispatcher(k8sClient, appRev1)
+			rtForAppV1, err := dp.Dispatch(ctx, []*unstructured.Unstructured{deploy1, svc1, pv1})
+			Expect(err).Should(BeNil())
+			By("Verify resources are applied successfully")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, &corev1.PersistentVolume{})).Should(Succeed())
+
+			By("Dispatch application revision 2 with v1 as latest RT")
+			dp2 := NewAppManifestsDispatcher(k8sClient, appRev2)
+			By("Ask dispatcher to skip GC")
+			dp2 = dp2.EnableUpgradeAndSkipGC(rtForAppV1)
+			_, err = dp2.Dispatch(ctx, []*unstructured.Unstructured{deploy2, svc2, pv2})
+			Expect(err).Should(BeNil())
+			By("Verify v2 resources are applied successfully")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName2, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName2, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName2}, &corev1.PersistentVolume{})).Should(Succeed())
+
+			By("Verify v1 resources still exist (skip GC)")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: deployName1, Namespace: namespace}, &appsv1.Deployment{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: svcName1, Namespace: namespace}, &corev1.Service{})).Should(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: pvName1}, &corev1.PersistentVolume{})).Should(Succeed())
+
+			By("Verify v1 resource tracker still exist (skip GC)")
+			Expect(k8sClient.Get(ctx, client.ObjectKey{Name: ConstructResourceTrackerName(appRevName1, namespace)},
+				&v1beta1.ResourceTracker{})).Should(Succeed())
+		})
+	})
+})
+
+// in envtest, no gc controller can delete PersistentVolume because of its finalizer
+// so we just use deletion timestamp to verify its deletion
+func persistentVolumeIsDeleted(pv *corev1.PersistentVolume) bool {
+	return !pv.ObjectMeta.DeletionTimestamp.IsZero()
+}
