4d563745dea67330bd74565613f567431c4dcf28
wangyike <wangyike.wyk@alibaba-inc.com>
2021-06-01 17:54:51 +0800

    cp oam pr 329
    fix Ci

--- pkg/controller/core.oam.dev/v1alpha2/applicationconfiguration/apply.go
+++ pkg/controller/core.oam.dev/v1alpha2/applicationconfiguration/apply.go
@@ -83,6 +83,9 @@ type workloads struct {
 }
 
 func (a *workloads) Apply(ctx context.Context, status []v1alpha2.WorkloadStatus, w []Workload, ao ...apply.ApplyOption) error {
+	if len(w) == 0 {
+		return errors.New("The number of workloads in appConfig is 0 ")
+	}
 	// they are all in the same namespace
 	var namespace = w[0].Workload.GetNamespace()
 	for _, wl := range w {
