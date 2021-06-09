62bb7deee285a9a7e177765b5e8528f1e5988156
wangyuan249 <249401644@qq.com>
2021-06-09 19:34:44 +0800

    fix interact init

--- e2e/plugin/plugin_test.go
+++ e2e/plugin/plugin_test.go
@@ -621,78 +621,78 @@ var componentWithDeepCue = `
 apiVersion: core.oam.dev/v1beta1
 kind: ComponentDefinition
 metadata:
-  name: mywebservice
-  namespace: default
-  annotations:
-    definition.oam.dev/description: "Describes long-running, scalable, containerized services that have a stable network endpoint to receive external network traffic from customers."
+ name: mywebservice
+ namespace: default
+ annotations:
+   definition.oam.dev/description: "Describes long-running, scalable, containerized services that have a stable network endpoint to receive external network traffic from customers."
 spec:
-  workload:
-    definition:
-      apiVersion: apps/v1
-      kind: Deployment
-  schematic:
-    cue:
-      template: |
-        output: {
-        	apiVersion: "apps/v1"
-        	kind:       "Deployment"
-        	spec: {
-        		selector: matchLabels: {
-        			"app.oam.dev/component": context.name
-        			if parameter.addRevisionLabel {
-        				"app.oam.dev/appRevision": context.appRevision
-        			}
-        		}
+ workload:
+   definition:
+     apiVersion: apps/v1
+     kind: Deployment
+ schematic:
+   cue:
+     template: |
+       output: {
+       	apiVersion: "apps/v1"
+       	kind:       "Deployment"
+       	spec: {
+       		selector: matchLabels: {
+       			"app.oam.dev/component": context.name
+       			if parameter.addRevisionLabel {
+       				"app.oam.dev/appRevision": context.appRevision
+       			}
+       		}
 
-        		template: {
-        			metadata: labels: {
-        				"app.oam.dev/component": context.name
-        				if parameter.addRevisionLabel {
-        					"app.oam.dev/appRevision": context.appRevision
-        				}
-        			}
+       		template: {
+       			metadata: labels: {
+       				"app.oam.dev/component": context.name
+       				if parameter.addRevisionLabel {
+       					"app.oam.dev/appRevision": context.appRevision
+       				}
+       			}
 
-        			spec: {
-        				containers: [{
-        					name:  context.name
-        					image: parameter.image
+       			spec: {
+       				containers: [{
+       					name:  context.name
+       					image: parameter.image
 
-        					if parameter["env"] != _|_ {
-        						env: parameter.env
-        					}
-        				}]
-        		    }
-        	    }
-            }
-        }
-        parameter: {
-        	// +usage=Which image would you like to use for your service
-        	// +short=i
-        	image: string
+       					if parameter["env"] != _|_ {
+       						env: parameter.env
+       					}
+       				}]
+       		    }
+       	    }
+           }
+       }
+       parameter: {
+       	// +usage=Which image would you like to use for your service
+       	// +short=i
+       	image: string
 
-        	// +ignore
-        	// +usage=If addRevisionLabel is true, the appRevision label will be added to the underlying pods
-        	addRevisionLabel: *false | bool
+       	// +ignore
+       	// +usage=If addRevisionLabel is true, the appRevision label will be added to the underlying pods
+       	addRevisionLabel: *false | bool
 
-        	// +usage=Define arguments by using environment variables
-        	env?: [...{
-        		// +usage=Environment variable name
-        		name: string
-        		// +usage=The value of the environment variable
-        		value?: string
-        		// +usage=Specifies a source the value of this var should come from
-        		valueFrom?: {
-        			// +usage=Selects a key of a secret in the pod's namespace
-        			secretKeyRef: {
-        				// +usage=The name of the secret in the pod's namespace to select from
-        				name: string
-                        // +ignore
-        				// +usage=The key of the secret to select from. Must be a valid secret key
-        				mySecretKey: string
-        			}
-        		}
-        	}]
-        }
+       	// +usage=Define arguments by using environment variables
+       	env?: [...{
+       		// +usage=Environment variable name
+       		name: string
+       		// +usage=The value of the environment variable
+       		value?: string
+       		// +usage=Specifies a source the value of this var should come from
+       		valueFrom?: {
+       			// +usage=Selects a key of a secret in the pod's namespace
+       			secretKeyRef: {
+       				// +usage=The name of the secret in the pod's namespace to select from
+       				name: string
+                       // +ignore
+       				// +usage=The key of the secret to select from. Must be a valid secret key
+       				mySecretKey: string
+       			}
+       		}
+       	}]
+       }
 `
 
 var dryRunResult = `---
