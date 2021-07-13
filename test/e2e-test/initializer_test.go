3af48d27b0c54b9809a006218b836a1e879c140e
qiaozp <chivalry.pp@gmail.com>
2021-07-13 19:39:07 +0800

    unfocus

--- test/e2e-test/initializer_test.go
+++ test/e2e-test/initializer_test.go
@@ -123,7 +123,7 @@ var _ = Describe("Initializer Normal tests", func() {
 		}, 30*time.Second, 5*time.Second).Should(Succeed())
 	})
 
-	FIt("Test apply initializer depends on other initializer", func() {
+	It("Test apply initializer depends on other initializer", func() {
 		compName := "env2-comp"
 
 		init := &v1beta1.Initializer{
