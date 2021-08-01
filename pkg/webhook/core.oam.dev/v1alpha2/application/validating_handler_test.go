00619cd9a2b6614a5d2ca87c8cc1ec2a11f578f8
LeoLiuYan <929908264@qq.com>
2021-08-01 19:13:31 +0800

    Add unit tests for webhook
    Signed-off-by: LeoLiuYan <929908264@qq.com>

--- pkg/webhook/core.oam.dev/v1alpha2/application/validating_handler_test.go
+++ pkg/webhook/core.oam.dev/v1alpha2/application/validating_handler_test.go
@@ -20,9 +20,12 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	admissionv1 "k8s.io/api/admission/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/oam-dev/kubevela/pkg/oam"
 )
 
 var _ = Describe("Test Application Validator", func() {
@@ -141,6 +144,82 @@ var _ = Describe("Test Application Validator", func() {
 "properties":{"cmd":["./podinfo","stress-cpu=3.0"],
 "image":"stefanprodan/podinfo:4.0.6","port":8080}}],
 "rolloutPlan":{"rolloutStrategy":"IncreaseFirst","targetSize":3}}}
+`),
+				},
+			},
+		}
+		resp := handler.Handle(ctx, req)
+		Expect(resp.Allowed).Should(BeFalse())
+	})
+
+	It("Test Application Validator external revision name [allow]", func() {
+		Expect(k8sClient.Create(ctx, &appsv1.ControllerRevision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "external-comp1",
+				Labels: map[string]string{
+					oam.LabelControllerRevisionComponent: "myworker",
+					oam.LabelComponentRevisionHash:       "81796829364afe1",
+				},
+			},
+			Data: runtime.RawExtension{
+				Raw: []byte(`
+{"apiVersion":"core.oam.dev/v1alpha2",
+"kind":"Component",
+"metadata":{"name":"myweb"},
+"spec":{"workload":{"apiVersion":"apps/v1",
+"kind":"Deployment",
+"spec": {"containers":[{"image":"stefanprodan/podinfo:4.0.6"}]}}}}
+`)},
+			Revision: 1,
+		})).Should(BeNil())
+
+		req := admission.Request{
+			AdmissionRequest: admissionv1beta1.AdmissionRequest{
+				Operation: admissionv1beta1.Create,
+				Resource:  metav1.GroupVersionResource{Group: "core.oam.dev", Version: "v1alpha2", Resource: "applications"},
+				Object: runtime.RawExtension{
+					Raw: []byte(`
+{"kind":"Application","metadata":{"name":"test-external-revision", "namespace":"default"},
+"spec":{"components":[{"name":"myworker","type":"worker",
+"properties":{"image":"stefanprodan/podinfo:4.0.6"},
+"externalRevision":"external-comp1"}]}}
+`),
+				},
+			},
+		}
+		resp := handler.Handle(ctx, req)
+		Expect(resp.Allowed).Should(BeTrue())
+	})
+
+	It("Test Application Validator external revision name [error]", func() {
+		Expect(k8sClient.Update(ctx, &appsv1.ControllerRevision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "external-comp1",
+			},
+			Data: runtime.RawExtension{
+				Raw: []byte(`
+{"apiVersion":"core.oam.dev/v1alpha2",
+"kind":"Component",
+"metadata":{"name":"myweb"},
+"spec":{"workload":{"apiVersion":"apps/v1",
+"kind":"Deployment",
+"spec": {"containers":[{"image":"stefanprodan/podinfo:4.0.6"}]}}}}
+`)},
+			Revision: 1,
+		})).Should(BeNil())
+
+		req := admission.Request{
+			AdmissionRequest: admissionv1beta1.AdmissionRequest{
+				Operation: admissionv1beta1.Create,
+				Resource:  metav1.GroupVersionResource{Group: "core.oam.dev", Version: "v1alpha2", Resource: "applications"},
+				Object: runtime.RawExtension{
+					Raw: []byte(`
+{"kind":"Application","metadata":{"name":"test-external-revision", "namespace":"default"},
+"spec":{"components":[{"name":"myworker","type":"worker",
+"properties":{"image":"stefanprodan/podinfo:4.0.6"},
+"externalRevision":"external-comp1"}]}}
 `),
 				},
 			},
