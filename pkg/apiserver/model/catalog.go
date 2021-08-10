110ba1e6472f5e5a861f2be378a93ae6ede6408a
Hongchao Deng <hongchaodeng1@gmail.com>
2021-08-10 10:30:07 +0800

    fix ci

--- pkg/apiserver/model/catalog.go
+++ pkg/apiserver/model/catalog.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package model
 
 // Catalog defines the data model of a Catalog
@@ -9,7 +25,7 @@ type Catalog struct {
 	// Type of the Catalog, such as "github" for a github repo.
 	Type string `json:"type,omitempty"`
 	// URL of the Catalog.
-	Url string `json:"url,omitempty"`
+	URL string `json:"url,omitempty"`
 	// Auth token used to sync Catalog.
 	Token string `json:"token,omitempty"`
 }
