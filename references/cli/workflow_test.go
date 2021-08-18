a6753737b3c41a8736f42705f6fea82576868f26
Tianxin Dong <dongtianxin.tx@alibaba-inc.com>
2021-08-18 17:04:21 +0800

    Feat(vela): add vela workflow resume command and ut (#2114)
    * Feat(vela): add vela workflow resume command and ut
    
    * fix ut

--- references/cli/workflow_test.go
+++ references/cli/workflow_test.go
@@ -35,8 +35,6 @@ import (
 func TestWorkflowSuspend(t *testing.T) {
 	c := initArgs()
 	ioStream := cmdutil.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
-	cmd := NewWorkflowSuspendCommand(c, ioStream)
-	initCommand(cmd)
 	ctx := context.TODO()
 
 	testCases := map[string]struct {
@@ -87,6 +85,8 @@ func TestWorkflowSuspend(t *testing.T) {
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			r := require.New(t)
+			cmd := NewWorkflowSuspendCommand(c, ioStream)
+			initCommand(cmd)
 
 			if tc.app != nil {
 				err := c.Client.Create(ctx, tc.app)
@@ -108,6 +108,113 @@ func TestWorkflowSuspend(t *testing.T) {
 			}, wf)
 			r.NoError(err)
 			r.Equal(true, wf.Status.Workflow.Suspend)
+		})
+	}
+}
+
+func TestWorkflowResume(t *testing.T) {
+	c := initArgs()
+	ioStream := cmdutil.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+	ctx := context.TODO()
+
+	testCases := map[string]struct {
+		app         *v1beta1.Application
+		expectedErr error
+	}{
+		"no app name specified": {
+			expectedErr: fmt.Errorf("must specify application name"),
+		},
+		"no workflow in app": {
+			app: &v1beta1.Application{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "no-workflow",
+					Namespace: "default",
+				},
+			},
+			expectedErr: fmt.Errorf("the application must have workflow"),
+		},
+		"workflow not suspended": {
+			app: &v1beta1.Application{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "workflow-not-suspended",
+					Namespace: "default",
+				},
+				Spec: v1beta1.ApplicationSpec{
+					Components: []common.ApplicationComponent{{
+						Name:       "test-component",
+						Type:       "worker",
+						Properties: runtime.RawExtension{Raw: []byte(`{"cmd":["sleep","1000"],"image":"busybox"}`)},
+					}},
+					Workflow: &v1beta1.Workflow{
+						Steps: []v1beta1.WorkflowStep{{
+							Name:       "test-wf1",
+							Type:       "foowf",
+							Properties: runtime.RawExtension{Raw: []byte(`{"namespace":"default"}`)},
+						}},
+					},
+				},
+				Status: common.AppStatus{
+					Workflow: &common.WorkflowStatus{
+						Suspend: false,
+					},
+				},
+			},
+		},
+		"resume successfully": {
+			app: &v1beta1.Application{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "workflow",
+					Namespace: "default",
+				},
+				Spec: v1beta1.ApplicationSpec{
+					Components: []common.ApplicationComponent{{
+						Name:       "test-component",
+						Type:       "worker",
+						Properties: runtime.RawExtension{Raw: []byte(`{"cmd":["sleep","1000"],"image":"busybox"}`)},
+					}},
+					Workflow: &v1beta1.Workflow{
+						Steps: []v1beta1.WorkflowStep{{
+							Name:       "test-wf1",
+							Type:       "foowf",
+							Properties: runtime.RawExtension{Raw: []byte(`{"namespace":"default"}`)},
+						}},
+					},
+				},
+				Status: common.AppStatus{
+					Workflow: &common.WorkflowStatus{
+						Suspend: true,
+					},
+				},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			r := require.New(t)
+			cmd := NewWorkflowResumeCommand(c, ioStream)
+			initCommand(cmd)
+
+			if tc.app != nil {
+				err := c.Client.Create(ctx, tc.app)
+				r.NoError(err)
+
+				cmd.SetArgs([]string{tc.app.Name})
+			}
+			err := cmd.Execute()
+			if tc.expectedErr != nil {
+				r.Equal(tc.expectedErr, err)
+				return
+			}
+			r.NoError(err)
+
+			wf := &v1beta1.Application{}
+			err = c.Client.Get(ctx, types.NamespacedName{
+				Namespace: tc.app.Namespace,
+				Name:      tc.app.Name,
+			}, wf)
+			r.NoError(err)
+			r.Equal(false, wf.Status.Workflow.Suspend)
 		})
 	}
 }
