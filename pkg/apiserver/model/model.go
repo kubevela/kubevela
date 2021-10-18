d2dc9a8da799cf2cc8bbe78ac57d14d11d0c6896
barnettZQG <576501057@qq.com>
2021-10-18 10:46:16 +0800

    Feat: application operation API implementation (#2478)
    * Feat: application operation API implementation
    
    * Docs: update swagger json
    
    * Docs: update swagger config
    
    * Feat: improve application management API implementation and testing
    
    * Style: change code style
    
    * Style: change some code style
    
    Co-authored-by: barnettZQG <yiyun.pro>

--- pkg/apiserver/model/model.go
+++ pkg/apiserver/model/model.go
@@ -19,8 +19,12 @@ package model
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/yaml"
+
+	"github.com/oam-dev/kubevela/pkg/apiserver/log"
 )
 
 var tableNamePrefix = "vela_"
@@ -29,11 +33,64 @@ var tableNamePrefix = "vela_"
 type JSONStruct map[string]interface{}
 
 // NewJSONStruct new jsonstruct from runtime.RawExtension
-func NewJSONStruct(raw runtime.RawExtension) (*JSONStruct, error) {
+func NewJSONStruct(raw *runtime.RawExtension) (*JSONStruct, error) {
 	var data JSONStruct
 	err := json.Unmarshal(raw.Raw, &data)
 	if err != nil {
 		return nil, fmt.Errorf("parse raw data failure %w", err)
 	}
 	return &data, nil
+}
+
+// NewJSONStructByString new jsonstruct from string
+func NewJSONStructByString(source string) (*JSONStruct, error) {
+	if source == "" {
+		return nil, nil
+	}
+	var data JSONStruct
+	err := json.Unmarshal([]byte(source), &data)
+	if err != nil {
+		return nil, fmt.Errorf("parse raw data failure %w", err)
+	}
+	return &data, nil
+}
+
+// JSON Encoded as a JSON string
+func (j *JSONStruct) JSON() string {
+	b, err := json.Marshal(j)
+	if err != nil {
+		log.Logger.Errorf("json marshal failure %s", err.Error())
+	}
+	return string(b)
+}
+
+// RawExtension Encoded as a RawExtension
+func (j *JSONStruct) RawExtension() *runtime.RawExtension {
+	yamlByte, err := yaml.Marshal(j)
+	if err != nil {
+		log.Logger.Errorf("yaml marshal failure %s", err.Error())
+		return nil
+	}
+	b, err := yaml.YAMLToJSON(yamlByte)
+	if err != nil {
+		log.Logger.Errorf("yaml to json failure %s", err.Error())
+		return nil
+	}
+	return &runtime.RawExtension{Raw: b}
+}
+
+// Model common model
+type Model struct {
+	CreateTime time.Time `json:"createTime"`
+	UpdateTime time.Time `json:"updateTime"`
+}
+
+// SetCreateTime set create time
+func (m *Model) SetCreateTime(time time.Time) {
+	m.CreateTime = time
+}
+
+// SetUpdateTime set update time
+func (m *Model) SetUpdateTime(time time.Time) {
+	m.UpdateTime = time
 }
---
+++ pkg/apiserver/rest/usecase/workflow.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2021 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package usecase
+
+import (
+	"context"
+	"errors"
+
+	"github.com/oam-dev/kubevela/pkg/apiserver/datastore"
+	"github.com/oam-dev/kubevela/pkg/apiserver/log"
+	"github.com/oam-dev/kubevela/pkg/apiserver/model"
+	apisv1 "github.com/oam-dev/kubevela/pkg/apiserver/rest/apis/v1"
+	"github.com/oam-dev/kubevela/pkg/apiserver/rest/utils/bcode"
+)
+
+// WorkflowUsecase workflow manage api
+type WorkflowUsecase interface {
+	GetWorkflow(ctx context.Context, workflowName string) (*model.Workflow, error)
+	DeleteWorkflow(ctx context.Context, workflowName string) error
+	CreateOrUpdateWorkflow(ctx context.Context, req apisv1.UpdateWorkflowRequest) (*apisv1.DetailWorkflowResponse, error)
+}
+
+// NewWorkflowUsecase new workflow usecase
+func NewWorkflowUsecase(ds datastore.DataStore) WorkflowUsecase {
+	return &workflowUsecaseImpl{ds: ds}
+}
+
+type workflowUsecaseImpl struct {
+	ds datastore.DataStore
+}
+
+// DeleteWorkflow delete application workflow
+func (w *workflowUsecaseImpl) DeleteWorkflow(ctx context.Context, workflowName string) error {
+	var workflow = &model.Workflow{
+		Name: workflowName,
+	}
+	if err := w.ds.Delete(ctx, workflow); err != nil {
+		if errors.Is(err, datastore.ErrRecordNotExist) {
+			return bcode.ErrWorkflowNotExist
+		}
+		return err
+	}
+	return nil
+}
+
+func (w *workflowUsecaseImpl) CreateOrUpdateWorkflow(ctx context.Context, req apisv1.UpdateWorkflowRequest) (*apisv1.DetailWorkflowResponse, error) {
+	var steps []model.WorkflowStep
+	for _, step := range req.Steps {
+		properties, err := model.NewJSONStructByString(step.Properties)
+		if err != nil {
+			log.Logger.Errorf("parse trait properties failire %w", err)
+			return nil, bcode.ErrInvalidProperties
+		}
+		steps = append(steps, model.WorkflowStep{
+			Name:       step.Name,
+			Type:       step.Type,
+			DependsOn:  step.DependsOn,
+			Inputs:     step.Inputs,
+			Outputs:    step.Outputs,
+			Properties: properties,
+		})
+	}
+	var workflow = model.Workflow{
+		Steps:     steps,
+		Name:      req.Name,
+		Namespace: req.Namespace,
+		Enable:    req.Enable,
+	}
+	if err := w.ds.Add(ctx, &workflow); err != nil {
+		return nil, err
+	}
+	return w.DetailWorkflow(ctx, &workflow)
+}
+
+// DetailWorkflow detail workflow
+func (w *workflowUsecaseImpl) DetailWorkflow(ctx context.Context, workflow *model.Workflow) (*apisv1.DetailWorkflowResponse, error) {
+	var steps []apisv1.WorkflowStep
+	for _, step := range workflow.Steps {
+		apiStep := apisv1.WorkflowStep{
+			Name:       step.Name,
+			Type:       step.Type,
+			DependsOn:  step.DependsOn,
+			Inputs:     step.Inputs,
+			Outputs:    step.Outputs,
+			Properties: step.Properties.JSON(),
+		}
+		if step.Properties != nil {
+			apiStep.Properties = step.Properties.JSON()
+		}
+		steps = append(steps, apiStep)
+	}
+	return &apisv1.DetailWorkflowResponse{Steps: steps, Enable: workflow.Enable}, nil
+}
+
+// GetWorkflow get workflow model
+func (w *workflowUsecaseImpl) GetWorkflow(ctx context.Context, workflowName string) (*model.Workflow, error) {
+	var workflow = model.Workflow{
+		Name: workflowName,
+	}
+	if err := w.ds.Get(ctx, &workflow); err != nil {
+		return nil, err
+	}
+	return &workflow, nil
+}
