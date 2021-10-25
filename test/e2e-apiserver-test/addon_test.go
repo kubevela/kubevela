01e216fb872bc11a497bff64e5425aa042487fdb
qiaozp <chivalry.pp@gmail.com>
2021-10-25 11:05:25 +0800

    add post func

--- test/e2e-apiserver-test/addon_test.go
+++ test/e2e-apiserver-test/addon_test.go
@@ -12,23 +12,28 @@ import (
 	apis "github.com/oam-dev/kubevela/pkg/apiserver/rest/apis/v1"
 )
 
+const baseURL = "http://127.0.0.1:8000"
+
+func post(path string, body interface{}) *http.Response {
+	b, err := json.Marshal(body)
+	Expect(err).Should(BeNil())
+
+	res, err := http.Post(baseURL+path, "application/json", bytes.NewBuffer(b))
+	Expect(err).Should(BeNil())
+	return res
+}
+
 var _ = Describe("Test addon rest api", func() {
 	It("should create and delete an addon registry", func() {
 		defer GinkgoRecover()
-		var req = apis.CreateAddonRegistryRequest{
+		req := apis.CreateAddonRegistryRequest{
 			Name: "test-addon-registry-1",
 			Git: &model.GitAddonSource{
 				URL: "test_url",
 				Dir: "test_dir",
 			},
 		}
-
-		bodyByte, err := json.Marshal(req)
-		Expect(err).Should(BeNil())
-
-		res, err := http.Post("http://127.0.0.1:8000/api/v1/addon_registries", "application/json", bytes.NewBuffer(bodyByte))
-		Expect(err).Should(BeNil())
-
+		res := post("/api/v1/addon_registries", req)
 		Expect(res).ShouldNot(BeNil())
 		Expect(res.StatusCode).Should(Equal(200))
 		Expect(res.Body).ShouldNot(BeNil())
@@ -36,7 +41,7 @@ var _ = Describe("Test addon rest api", func() {
 		defer res.Body.Close()
 
 		var rmeta apis.AddonRegistryMeta
-		err = json.NewDecoder(res.Body).Decode(&rmeta)
+		err := json.NewDecoder(res.Body).Decode(&rmeta)
 
 		Expect(err).Should(BeNil())
 		Expect(rmeta.Name).Should(Equal(req.Name))
@@ -45,5 +50,26 @@ var _ = Describe("Test addon rest api", func() {
 
 	It("should list all addons", func() {})
 
-	It("should enable and disable an addon", func() {})
+	It("should enable and disable an addon", func() {
+		defer GinkgoRecover()
+		req := apis.EnableAddonRequest{
+			Args: map[string]string{},
+		}
+		res := post("/api/v1/addons/enable?name=fluxcd", req)
+		Expect(res).ShouldNot(BeNil())
+		Expect(res.StatusCode).Should(Equal(200))
+		Expect(res.Body).ShouldNot(BeNil())
+
+		defer res.Body.Close()
+
+		var statusRes apis.AddonStatusResponse
+		err := json.NewDecoder(res.Body).Decode(&statusRes)
+
+		Expect(err).Should(BeNil())
+		Expect(statusRes.Phase).Should(Equal(apis.AddonPhaseEnabling))
+
+		// Wait for addon enabled
+
+
+	})
 })
