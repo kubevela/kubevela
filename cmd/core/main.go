77db060f8ed18f9e17899ae602ef31572aa770d3
Jianbo Sun <jianbo.sjb@alibaba-inc.com>
2021-11-04 18:00:35 +0800

    Fix: show reconcile error log

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -32,6 +32,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/klog/v2"
+	"k8s.io/klog/v2/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
@@ -192,7 +193,7 @@ func main() {
 			os.Exit(1)
 		}
 	}
-
+	ctrl.SetLogger(klogr.New())
 	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
 		Scheme:                     scheme,
 		MetricsBindAddress:         metricsAddr,
