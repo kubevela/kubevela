50cfe0c68f4d16e5f26fd4a38764f61a6f3d2d69
Tianxin Dong <dongtianxin.tx@alibaba-inc.com>
2021-11-05 16:07:04 +0800

    Fix: allow definition schema cm can be same name in different definition type (#2618)
    * Fix: fix definition schema cm name
    
    * fix ut
    
    * fix ut
    
    * fix show
    
    * add switch default case

--- test/e2e-test/helm_app_test.go
+++ test/e2e-test/helm_app_test.go
@@ -372,7 +372,7 @@ var _ = Describe("Test application containing helm module", func() {
 
 	It("Test store JSON schema of Helm Chart in ConfigMap", func() {
 		By("Get the ConfigMap")
-		cmName := fmt.Sprintf("schema-%s", cdName)
+		cmName := fmt.Sprintf("component-schema-%s", cdName)
 		Eventually(func() error {
 			cm := &corev1.ConfigMap{}
 			if err := k8sClient.Get(ctx, client.ObjectKey{Name: cmName, Namespace: namespace}, cm); err != nil {
