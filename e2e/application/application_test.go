e5745bb3f33b03896bc74941b8f634bdf90e4a7f
Jianbo Sun <jianbo.sjb@alibaba-inc.com>
2021-11-07 08:54:48 +0800

    Feat: add vela exec for multi cluster (#2299)
    fix
    
    support vela exec

--- e2e/application/application_test.go
+++ e2e/application/application_test.go
@@ -49,11 +49,11 @@ var _ = ginkgo.Describe("Test Vela Application", func() {
 	e2e.EnvInitContext("env init", envName)
 	e2e.EnvSetContext("env set", envName)
 	e2e.JsonAppFileContext("deploy app-basic", appbasicJsonAppFile)
+	ApplicationExecContext("exec -- COMMAND", applicationName)
 	e2e.JsonAppFileContext("update app-basic, add scaler trait with replicas 2", appbasicAddTraitJsonAppFile)
 	e2e.ComponentListContext("ls", applicationName, workloadType, traitAlias)
 	ApplicationStatusContext("status", applicationName, workloadType)
 	ApplicationStatusDeeplyContext("status", applicationName, workloadType, envName)
-	ApplicationExecContext("exec -- COMMAND", applicationName)
 	// ApplicationPortForwardContext("port-forward", applicationName)
 	e2e.WorkloadDeleteContext("delete", applicationName)
 
