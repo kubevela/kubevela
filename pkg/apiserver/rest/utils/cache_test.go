aeaff573189870f3646d6589c4087470a9b82db4
Hongchao Deng <hongchaodeng1@gmail.com>
2021-11-09 11:14:13 +0800

    Fix: cache IsExpired()

---
+++ pkg/apiserver/rest/utils/cache_test.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test cache utils", func() {
+	It("should return false for IsExpired()", func() {
+		c := NewMemoryCache("test", 10*time.Hour)
+		Expect(c.IsExpired()).ShouldNot(BeFalse())
+	})
+})
