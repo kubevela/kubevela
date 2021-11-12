6c0b943dfcbe6aa18f21eb702b84e115017003a7
Zheng Xi Zhou <zzxwill@gmail.com>
2021-11-12 22:33:09 +0800

    Fix: application status.services doesn't include Terraform typed components (#2692)
    Function aggregateHealthStatus() in pkg/controller/core.oam.dev/v1alpha2/
    application/apply.go which is used to retrieve components status, was
    abandoned. All unit-tests of it was abandoned too. Fixed it and restore all the unit
    tests.

---
+++ test/e2e-addon-test/suite_test.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2021 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers_test
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/oam-dev/kubevela/apis/standard.oam.dev/v1alpha1"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	crdv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	core "github.com/oam-dev/kubevela/apis/core.oam.dev"
+	// +kubebuilder:scaffold:imports
+)
+
+var k8sClient client.Client
+var scheme = runtime.NewScheme()
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"Addons Controller Suite",
+		[]Reporter{printer.NewlineReporter{}})
+}
+
+var _ = BeforeSuite(func(done Done) {
+	By("Bootstrapping test environment")
+	rand.Seed(time.Now().UnixNano())
+	logf.SetLogger(zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter)))
+	err := clientgoscheme.AddToScheme(scheme)
+	Expect(err).Should(BeNil())
+	err = core.AddToScheme(scheme)
+	Expect(err).Should(BeNil())
+	err = crdv1.AddToScheme(scheme)
+	Expect(err).Should(BeNil())
+	err = v1alpha1.AddToScheme(scheme)
+	Expect(err).Should(BeNil())
+	By("Setting up kubernetes client")
+	k8sClient, err = client.New(config.GetConfigOrDie(), client.Options{Scheme: scheme})
+	if err != nil {
+		logf.Log.Error(err, "failed to create k8sClient")
+		Fail("setup failed")
+	}
+	By("Finished setting up test environment")
+	close(done)
+}, 300)
+
+var _ = AfterSuite(func() {
+	By("Tearing down test environment")
+	// TearDownSuite()
+	By("Finished tearing down test environment")
+})
