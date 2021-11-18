c83456cf534961fe83d367fd25318a62ae783a98
FogDong <dongtianxin.tx@alibaba-inc.com>
2021-11-18 22:31:10 +0800

    fix rebase

--- pkg/velaql/providers/query/collector.go
+++ pkg/velaql/providers/query/collector.go
@@ -77,7 +77,7 @@ func (c *AppCollector) CollectLatestResourceFromApp() ([]AppResources, error) {
 	if app.Status.LatestRevision != nil {
 		revision = app.Status.LatestRevision.Revision
 	}
-	publishVersion := app.GetAnnotations()[types.AnnotationPublishVersion]
+	publishVersion := app.GetAnnotations()[oam.AnnotationPublishVersion]
 
 	appRevName := fmt.Sprintf("%s-v%d", app.Name, revision)
 	comps := make(map[string][]Resource, len(app.Spec.Components))
