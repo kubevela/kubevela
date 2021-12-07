c38874aba842f543b6845295548f03eec78117ac
Zheng Xi Zhou <zzxwill@gmail.com>
2021-12-07 10:55:59 +0800

    add e2e test case
    Signed-off-by: Zheng Xi Zhou <zzxwill@gmail.com>

--- test/e2e-addon-test/addon_test.go
+++ test/e2e-addon-test/addon_test.go
@@ -122,6 +122,7 @@ var _ = Describe("Addon tests", func() {
 			time.Second*30, time.Millisecond*500).ShouldNot(BeNil())
 	})
 
+
 	It("Addon observability is successfully enabled", func() {
 		By("Install Addon Observability")
 		output, err := exec.Command("bash", "-c", "/tmp/vela addon enable observability grafana-domain=abc.com").Output()
