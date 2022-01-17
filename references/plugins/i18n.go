670a327161927e49dc56ac10aa29f58961cb1e88
Zheng Xi Zhou <zzxwill@gmail.com>
2022-01-17 23:16:48 +0800

    Fix: add app samples for Terraform definition
    Added application samples for all Terraform typed ComponentDefinition,
    and also localize the title for AWS and AZure
    
    Signed-off-by: Zheng Xi Zhou <zzxwill@gmail.com>

--- references/plugins/i18n.go
+++ references/plugins/i18n.go
@@ -44,6 +44,14 @@ var Definitions = map[string]map[Language]string{
 		Zh: "阿里云",
 		En: "Alibaba Cloud",
 	},
+	"AWS": {
+		Zh: "AWS",
+		En: "AWS",
+	},
+	"Azure": {
+		Zh: "Azure",
+		En: "Azure",
+	},
 	"Name": {
 		Zh: "名称",
 		En: "Name",
@@ -75,5 +83,53 @@ var Definitions = map[string]map[Language]string{
 	"Terraform_configuration_for_Alibaba_Cloud_ACK_cluster": {
 		Zh: "用于部署阿里云 ACK 集群的组件说明",
 		En: "Terraform configuration for Alibaba Cloud ACK cluster",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_Serverless_Kubernetes_(ASK)": {
+		Zh: "用于部署阿里云 Serverless Kubernetes (ASK) 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud Serverless Kubernetes (ASK)",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_Elastic_IP": {
+		Zh: "用于部署阿里云弹性 IP 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud Elastic IP",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_OSS_object": {
+		Zh: "用于部署阿里云 OSS 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud OSS",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_RDS_object": {
+		Zh: "用于部署阿里云 RDS 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud RDS",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_Redis": {
+		Zh: "用于部署阿里云 Redis 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud Redis",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_SLS_Project": {
+		Zh: "用于部署阿里云 SLS Project 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud SLS Project",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_SLS_Store": {
+		Zh: "用于部署阿里云 SLS Store 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud SLS Store",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_VPC": {
+		Zh: "用于部署阿里云 VPC 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud VPC",
+	},
+	"Terraform_configuration_for_Alibaba_Cloud_VSwitch": {
+		Zh: "用于部署阿里云 VSwitch 的组件说明",
+		En: "Terraform configuration for Alibaba Cloud VSwitch",
+	},
+	"Terraform_configuration_for_AWS_S3": {
+		Zh: "用于部署 AWS S3 的组件说明",
+		En: "Terraform configuration for AWS S3",
+	},
+	"Terraform_configuration_for_Azure_Database_Mariadb": {
+		Zh: "用于部署 Azure mariadb 数据库的组件说明",
+		En: "Terraform configuration for Azure Database Mariadb",
+	},
+	"Terraform_configuration_for_Azure_Blob_Storage_Account": {
+		Zh: "用于部署 Azure Blob Storage 账号的的组件说明",
+		En: "Terraform configuration for Azure Blob Storage Account",
 	},
 }
