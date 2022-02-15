ddbcdfb2a669544225931b59313fae73d028f21e
barnettZQG <barnett.zqg@gmail.com>
2022-02-15 14:00:01 +0800

    Feat: change rollout image pull policy
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- makefiles/e2e.mk
+++ makefiles/e2e.mk
@@ -16,7 +16,7 @@ e2e-setup:
 	sh ./hack/e2e/modify_charts.sh
 	helm upgrade --install --create-namespace --namespace vela-system --set image.pullPolicy=IfNotPresent --set image.repository=vela-core-test --set applicationRevisionLimit=5 --set dependCheckWait=10s --set image.tag=$(GIT_COMMIT) --wait kubevela ./charts/vela-core
 	helm upgrade --install --create-namespace --namespace oam-runtime-system --set image.pullPolicy=IfNotPresent --set image.repository=vela-core-test --set dependCheckWait=10s --set image.tag=$(GIT_COMMIT) --wait oam-runtime ./charts/oam-runtime
-	helm upgrade --install --namespace vela-system --set image.tag=$(GIT_COMMIT) --wait oam-rollout --set image.repository=vela-runtime-rollout-test --set image.tag=$(GIT_COMMIT) ./runtime/rollout/charts
+	helm upgrade --install --namespace vela-system --wait oam-rollout --set image.repository=vela-runtime-rollout-test --set image.tag=$(GIT_COMMIT) ./runtime/rollout/charts
 
 	go run ./e2e/addon/mock &
 	bin/vela addon enable fluxcd
