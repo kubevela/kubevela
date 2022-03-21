5266562496192bc10a51542e8b962ff6b1b961a6
Zheng Xi Zhou <zzxwill@gmail.com>
2022-03-21 18:38:25 +0800

    support delete a provider
    Signed-off-by: Zheng Xi Zhou <zzxwill@gmail.com>

--- references/cli/provider.go
+++ references/cli/provider.go
@@ -20,37 +20,36 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/oam-dev/kubevela/pkg/definition"
-	"github.com/oam-dev/kubevela/references/plugins"
-	kerrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	k8stypes "k8s.io/apimachinery/pkg/types"
 	"strings"
 
 	"github.com/gosuri/uitable"
 	tcv1beta1 "github.com/oam-dev/terraform-controller/api/v1beta1"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8stypes "k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	coreapi "github.com/oam-dev/kubevela/apis/core.oam.dev/common"
 	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
 	"github.com/oam-dev/kubevela/apis/types"
-	pkgaddon "github.com/oam-dev/kubevela/pkg/addon"
+	"github.com/oam-dev/kubevela/pkg/definition"
 	"github.com/oam-dev/kubevela/pkg/oam"
 	"github.com/oam-dev/kubevela/pkg/utils/common"
 	cmdutil "github.com/oam-dev/kubevela/pkg/utils/util"
+	"github.com/oam-dev/kubevela/references/plugins"
 )
 
 const (
 
 	// ProviderNamespace is the namespace of Terraform Cloud Provider
 	ProviderNamespace = "default"
-	// ProviderTerraformProviderNameArgument is the argument name of addon terraform Provider
-	ProviderTerraformProviderNameArgument = "providerName"
 
 	labelVal = "terraform-provider"
+
+	providerNameParam = "name"
 )
 
 // NewProviderCommand create `addon` command
@@ -64,13 +63,18 @@ func NewProviderCommand(c common.Args, order string, ioStreams cmdutil.IOStreams
 			types.TagCommandType:  types.TypeExtension,
 		},
 	}
-	subCMDs, err := prepareProviderAddCommand(c)
+	add, err := prepareProviderAddCommand(c)
 	if err == nil {
-		cmd.AddCommand(subCMDs...)
+		cmd.AddCommand(add)
 	}
+
+	delete, err := prepareProviderDeleteCommand(c)
+	if err == nil {
+		cmd.AddCommand(delete)
+	}
+
 	cmd.AddCommand(
 		NewProviderListCommand(c),
-		//NewProviderAddCommand(c),
 	)
 	return cmd
 }
@@ -96,169 +100,138 @@ func NewProviderListCommand(c common.Args) *cobra.Command {
 	}
 }
 
-func prepareProviderAddCommand(c common.Args) ([]*cobra.Command, error) {
+func prepareProviderAddCommand(c common.Args) (*cobra.Command, error) {
 	ctx := context.Background()
 	k8sClient, err := c.GetClient()
 	if err != nil {
 		return nil, err
 	}
-	defs, err := getTerraformProviderTypes(ctx, k8sClient)
-	if err != nil || len(defs) == 0 {
-		cmd := &cobra.Command{
-			Use:     "add",
-			Short:   "add a Terraform Cloud Provider",
-			Long:    "add a Terraform Cloud Provider by creating a credential secret and a Terraform Controller Provider",
-			Example: "vela Provider add <Provider-type>",
-			RunE: func(cmd *cobra.Command, args []string) error {
-				if len(args) < 1 {
-					errMsg := "must specify a Terraform Cloud Provider type"
-					defs, err := getTerraformProviderTypes(ctx, k8sClient)
-					if err == nil && len(defs) > 0 {
-						providerDefNames := make([]string, len(defs))
-						for i, def := range defs {
-							providerDefNames[i] = def.Name
-						}
-						names := strings.Join(providerDefNames, ", ")
-						errMsg += fmt.Sprintf(": select one from %s", names)
-					}
-					return errors.New(errMsg)
-				}
-				return nil
-			},
-		}
-		return []*cobra.Command{cmd}, err
-	}
-	cmds := make([]*cobra.Command, len(defs))
-	for i, d := range defs {
-		providerType := d.Name
-		cmd := &cobra.Command{
-			Use:     fmt.Sprintf("add %s", providerType),
-			Short:   fmt.Sprintf("authenticate Terraform Cloud Provider %s", providerType),
-			Long:    fmt.Sprintf("authenticate Terraform Cloud Provider %s by creating a credential secret and a Terraform Controller Provider", providerType),
-			Example: fmt.Sprintf("vela provider add %s", providerType),
-		}
-		parameters, err := getParameters(ctx, k8sClient, providerType)
-		if err != nil {
-			return nil, err
-		}
-		for _, p := range parameters {
-			cmd.Flags().String(p.Name, fmt.Sprint(p.Default), p.Usage)
-		}
-		cmd.RunE = func(cmd *cobra.Command, args []string) error {
-			name, err := cmd.Flags().GetString("name")
-			if err != nil || name == "" {
-				return errors.New("must specify a name for the Terraform Cloud Provider")
-			}
-			var properties = make(map[string]string, len(parameters))
-			for _, p := range parameters {
-				value, err := cmd.Flags().GetString(p.Name)
-				if err != nil {
-					return err
-				}
-				if value == "" {
-					return fmt.Errorf("must specify a value for %s", p.Name)
-				}
-				properties[p.Name] = value
-			}
-			data, err := json.Marshal(properties)
-			if err != nil {
-				return fmt.Errorf("failed to authentiate Terraform cloud provier %s", providerType)
-			}
-			providerAppName := fmt.Sprintf("config-terraform-provider-%s", name)
-			a := &v1beta1.Application{}
-			if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: types.DefaultKubeVelaNS, Name: providerAppName}, a); err != nil {
-				if kerrors.IsNotFound(err) {
-					a = &v1beta1.Application{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      providerAppName,
-							Namespace: types.DefaultKubeVelaNS,
-						},
-						Spec: v1beta1.ApplicationSpec{
-							Components: []coreapi.ApplicationComponent{
-								{
-									Name: providerAppName,
-									Type: d.Name,
-									Properties: &runtime.RawExtension{
-										Raw: data,
-									},
-								},
-							},
-						},
-					}
-					if err := k8sClient.Create(ctx, a); err != nil {
-						return err
-					}
-				}
-			}
-			return fmt.Errorf("terraform provider %s for %s already exists", name, providerType)
-		}
-		cmds[i] = cmd
-	}
-	return cmds, nil
-}
 
-// NewProviderAddCommand create a Provider
-func NewProviderAddCommand(c common.Args) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "add",
-		Short:   "add a Terraform Cloud Provider",
-		Long:    "add a Terraform Cloud Provider by creating a credential secret and a Terraform Controller Provider",
-		Example: "vela Provider add <Provider-type>",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := context.Background()
-			k8sClient, err := c.GetClient()
-			if err != nil {
-				return err
-			}
-			switch {
-			case len(args) < 1:
-				errMsg := "must specify a Terraform Cloud Provider type"
-				defs, err := getTerraformProviderTypes(ctx, k8sClient)
-				if err == nil && len(defs) > 0 {
+		Short:   "Authenticate Terraform Cloud Provider",
+		Long:    "Authenticate Terraform Cloud Provider by creating a credential secret and a Terraform Controller Provider",
+		Example: "vela provider add <provider-type>",
+	}
+
+	addSubCommands, err := prepareProviderAddSubCommand(c)
+	if err != nil {
+		return nil, err
+	}
+	cmd.AddCommand(addSubCommands...)
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		defs, err := getTerraformProviderTypes(ctx, k8sClient)
+		if len(args) < 1 {
+			errMsg := "must specify a Terraform Cloud Provider type"
+			if err == nil {
+				if len(defs) > 0 {
 					providerDefNames := make([]string, len(defs))
 					for i, def := range defs {
 						providerDefNames[i] = def.Name
 					}
 					names := strings.Join(providerDefNames, ", ")
 					errMsg += fmt.Sprintf(": select one from %s", names)
+				} else {
+					errMsg += "\nNo Terraform Cloud Provider types exist. Please run `vela addon enable` first"
 				}
-				return errors.New(errMsg)
-			case len(args) >= 1:
-				// should also support `vela provider add <provider-type> -h`
-				providerType := args[0]
-				// validate whether providerType is a valid Terraform provider type
+			}
+			return errors.New(errMsg)
+		} else {
+			if err == nil {
 				var found bool
-				defs, err := getTerraformProviderTypes(ctx, k8sClient)
-				if err != nil {
-					return fmt.Errorf("failed to validate provider type: %s", providerType)
-				}
 				for _, def := range defs {
-					if def.Name == providerType {
+					if def.Name == args[0] {
 						found = true
-						break
 					}
 				}
 				if !found {
-					return fmt.Errorf("provider type: %s is invalid", providerType)
+					return fmt.Errorf("%s is not valid", args[0])
 				}
-
-				parameters, err := getParameters(ctx, k8sClient, providerType)
-				if err != nil {
-					return fmt.Errorf("failed to validate provider type: %s", providerType)
-				}
-				errMsg := fmt.Sprintf("must set properties for the Terraform Cloud Provider %s\n", providerType)
-				for _, p := range parameters {
-					cmd.Flags().String(p.Name, fmt.Sprint(p.Default), p.Usage)
-				}
-				cmd.HelpFunc()(cmd, args)
-				return errors.New(errMsg)
 			}
-
-			return nil
-		},
+		}
+		return nil
 	}
+	return cmd, nil
+}
 
-	return cmd
+func prepareProviderAddSubCommand(c common.Args) ([]*cobra.Command, error) {
+	ctx := context.Background()
+	k8sClient, err := c.GetClient()
+	if err != nil {
+		return nil, err
+	}
+	defs, err := getTerraformProviderTypes(ctx, k8sClient)
+	if err == nil {
+		cmds := make([]*cobra.Command, len(defs))
+		for i, d := range defs {
+			providerType := d.Name
+			cmd := &cobra.Command{
+				Use:     providerType,
+				Short:   fmt.Sprintf("Authenticate Terraform Cloud Provider %s", providerType),
+				Long:    fmt.Sprintf("Authenticate Terraform Cloud Provider %s by creating a credential secret and a Terraform Controller Provider", providerType),
+				Example: fmt.Sprintf("vela provider add %s", providerType),
+			}
+			parameters, err := getParameters(ctx, k8sClient, providerType)
+			if err != nil {
+				return nil, err
+			}
+			for _, p := range parameters {
+				cmd.Flags().String(p.Name, fmt.Sprint(p.Default), p.Usage)
+			}
+			cmd.RunE = func(cmd *cobra.Command, args []string) error {
+				name, err := cmd.Flags().GetString(providerNameParam)
+				if err != nil || name == "" {
+					return fmt.Errorf("must specify a name for the Terraform Cloud Provider %s", providerType)
+				}
+				var properties = make(map[string]string, len(parameters))
+				for _, p := range parameters {
+					value, err := cmd.Flags().GetString(p.Name)
+					if err != nil {
+						return err
+					}
+					if value == "" {
+						return fmt.Errorf("must specify a value for %s", p.Name)
+					}
+					properties[p.Name] = value
+				}
+				data, err := json.Marshal(properties)
+				if err != nil {
+					return fmt.Errorf("failed to authentiate Terraform cloud provier %s", providerType)
+				}
+				providerAppName := fmt.Sprintf("config-terraform-provider-%s", name)
+				a := &v1beta1.Application{}
+				if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: types.DefaultKubeVelaNS, Name: providerAppName}, a); err != nil {
+					if kerrors.IsNotFound(err) {
+						a = &v1beta1.Application{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      providerAppName,
+								Namespace: types.DefaultKubeVelaNS,
+							},
+							Spec: v1beta1.ApplicationSpec{
+								Components: []coreapi.ApplicationComponent{
+									{
+										Name: providerAppName,
+										Type: d.Name,
+										Properties: &runtime.RawExtension{
+											Raw: data,
+										},
+									},
+								},
+							},
+						}
+						if err := k8sClient.Create(ctx, a); err != nil {
+							return err
+						}
+					}
+				}
+				return fmt.Errorf("terraform provider %s for %s already exists", name, providerType)
+			}
+			cmds[i] = cmd
+		}
+		return cmds, nil
+	}
+	return nil, nil
 }
 
 // getParameters gets parameter from a Terraform Cloud Provider, ie the ComponentDefinition
@@ -274,14 +247,6 @@ func getParameters(ctx context.Context, k8sClient client.Client, providerType st
 	return cap.Parameters, nil
 }
 
-func prepareParameterFlags(parameters []types.Parameter) string {
-	var guide string
-	for _, p := range parameters {
-		guide += fmt.Sprintf("    -%s: Required: %v, Description: %s\n", p.Name, p.Required, p.Usage)
-	}
-	return guide
-}
-
 type Provider struct {
 	Type string
 	Name string
@@ -293,44 +258,37 @@ func listProviders(ctx context.Context, k8sClient client.Client) error {
 	tcProviders := &tcv1beta1.ProviderList{}
 	if err := k8sClient.List(ctx, tcProviders, client.InNamespace(ProviderNamespace),
 		client.MatchingLabels{"config.oam.dev/type": labelVal}); err != nil {
+		return errors.Wrap(err, "failed to retrieve providers")
+	}
+	defs, err := getTerraformProviderTypes(ctx, k8sClient)
+	if err != nil {
 		if kerrors.IsNotFound(err) {
-			defs, err := getTerraformProviderTypes(ctx, k8sClient)
-			if err != nil {
-				if kerrors.IsNotFound(err) {
-					return errors.New("no Terraform Cloud Provider found, please run `vela addon enable` first")
-				}
-				return errors.Wrap(err, "failed to retrieve providers")
-			}
-			for _, d := range defs {
-				providers = append(providers, Provider{
-					Type: d.Name,
-				})
-			}
+			return errors.New("no Terraform Cloud Provider found, please run `vela addon enable` first")
 		}
 		return errors.Wrap(err, "failed to retrieve providers")
 	}
-	if len(tcProviders.Items) == 0 {
-		defs := &v1beta1.ComponentDefinitionList{}
-		if err := k8sClient.List(ctx, defs, client.InNamespace(types.DefaultKubeVelaNS),
-			client.MatchingLabels{definition.UserPrefix + "type.config.oam.dev": labelVal}); err != nil {
-			return errors.Wrap(err, "failed to retrieve providers")
+
+	for _, d := range defs {
+		var found bool
+		for _, p := range tcProviders.Items {
+			if p.Labels[oam.WorkloadTypeLabel] == d.Name {
+				found = true
+				providers = append(providers, Provider{
+					Type: p.Labels[oam.WorkloadTypeLabel],
+					Name: p.Name,
+					Age:  p.CreationTimestamp.String(),
+				})
+			}
 		}
-		if len(defs.Items) == 0 {
-			return errors.New("no Terraform Cloud Provider found, please run `vela addon enable` first")
-		}
-		for _, d := range defs.Items {
+		if !found {
 			providers = append(providers, Provider{
 				Type: d.Name,
 			})
 		}
-	} else {
-		for _, p := range tcProviders.Items {
-			providers = append(providers, Provider{
-				Type: p.Labels[oam.WorkloadTypeLabel],
-				Name: p.Name,
-				Age:  p.CreationTimestamp.String(),
-			})
-		}
+	}
+
+	if len(providers) == 0 {
+		return errors.New("no Terraform Cloud Provider found, please run `vela addon enable` first")
 	}
 
 	table := uitable.New()
@@ -364,26 +322,109 @@ func getTerraformProviderType(ctx context.Context, k8sClient client.Client, name
 	return def, nil
 }
 
-// TransProviderName will turn addon's name from xxx/yyy to xxx-yyy
-func TransProviderName(name string) string {
-	return strings.ReplaceAll(name, "/", "-")
+func prepareProviderDeleteCommand(c common.Args) (*cobra.Command, error) {
+	ctx := context.Background()
+	k8sClient, err := c.GetClient()
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := &cobra.Command{
+		Use:     "delete",
+		Aliases: []string{"rm", "del"},
+		Short:   "Delete Terraform Cloud Provider",
+		Long:    "Delete Terraform Cloud Provider",
+		Example: "vela provider delete <provider-type> -name <provider-name>",
+	}
+
+	deleteSubCommands, err := prepareProviderDeleteSubCommand(c)
+	if err != nil {
+		return nil, err
+	}
+	cmd.AddCommand(deleteSubCommands...)
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		defs, err := getTerraformProviderTypes(ctx, k8sClient)
+		if len(args) < 1 {
+			errMsg := "must specify a Terraform Cloud Provider type"
+			if err == nil {
+				if len(defs) > 0 {
+					providerDefNames := make([]string, len(defs))
+					for i, def := range defs {
+						providerDefNames[i] = def.Name
+					}
+					names := strings.Join(providerDefNames, ", ")
+					errMsg += fmt.Sprintf(": select one from %s", names)
+				} else {
+					errMsg += "\nNo Terraform Cloud Provider types exist. Please run `vela addon enable` first"
+				}
+			}
+			return errors.New(errMsg)
+		} else {
+			if err == nil {
+				var found bool
+				for _, def := range defs {
+					if def.Name == args[0] {
+						found = true
+					}
+				}
+				if !found {
+					return fmt.Errorf("%s is not valid", args[0])
+				}
+			}
+		}
+		return nil
+	}
+	return cmd, nil
 }
 
-func mergeProviders(a1, a2 []*pkgaddon.UIData) []*pkgaddon.UIData {
-	for _, item := range a2 {
-		if hasProvider(a1, item.Name) {
-			continue
-		}
-		a1 = append(a1, item)
+func prepareProviderDeleteSubCommand(c common.Args) ([]*cobra.Command, error) {
+	ctx := context.Background()
+	k8sClient, err := c.GetClient()
+	if err != nil {
+		return nil, err
 	}
-	return a1
-}
-
-func hasProvider(addons []*pkgaddon.UIData, name string) bool {
-	for _, addon := range addons {
-		if addon.Name == name {
-			return true
+	defs, err := getTerraformProviderTypes(ctx, k8sClient)
+	if err == nil {
+		cmds := make([]*cobra.Command, len(defs))
+		for i, d := range defs {
+			providerType := d.Name
+			cmd := &cobra.Command{
+				Use:     providerType,
+				Short:   fmt.Sprintf("Delete Terraform Cloud Provider %s", providerType),
+				Long:    fmt.Sprintf("Delete Terraform Cloud Provider %s", providerType),
+				Example: fmt.Sprintf("vela provider delete %s", providerType),
+			}
+			parameters, err := getParameters(ctx, k8sClient, providerType)
+			if err != nil {
+				return nil, err
+			}
+			for _, p := range parameters {
+				if p.Name == providerNameParam {
+					cmd.Flags().String(p.Name, fmt.Sprint(p.Default), p.Usage)
+				}
+			}
+			cmd.RunE = func(cmd *cobra.Command, args []string) error {
+				name, err := cmd.Flags().GetString(providerNameParam)
+				if err != nil || name == "" {
+					return fmt.Errorf("must specify a name for the Terraform Cloud Provider %s", providerType)
+				}
+				providerAppName := fmt.Sprintf("config-terraform-provider-%s", name)
+				a := &v1beta1.Application{}
+				if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: types.DefaultKubeVelaNS, Name: providerAppName}, a); err != nil {
+					if kerrors.IsNotFound(err) {
+						return fmt.Errorf("provider %s for %s does not exist", name, providerType)
+					}
+				}
+				if err := k8sClient.Delete(ctx, a); err != nil {
+					return err
+				}
+				fmt.Printf("Successfully delete provider %s for %s\n", name, providerType)
+				return nil
+			}
+			cmds[i] = cmd
 		}
+		return cmds, nil
 	}
-	return false
+	return nil, nil
 }
