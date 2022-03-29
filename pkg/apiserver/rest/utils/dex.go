4e89347d34e293a83bdbe3f68a3a5e3442fba1e6
Zheng Xi Zhou <zzxwill@gmail.com>
2022-03-29 17:37:20 +0800

    fix ci
    Signed-off-by: Zheng Xi Zhou <zzxwill@gmail.com>

--- pkg/apiserver/rest/utils/dex.go
+++ pkg/apiserver/rest/utils/dex.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package utils
 
 import (
@@ -10,13 +26,7 @@ import (
 	"github.com/oam-dev/kubevela/apis/types"
 )
 
-//type Connectors struct {
-//	Type   string            `json:"type"`
-//	ID     string            `json:"id"`
-//	Name   string            `json:"name"`
-//	Config map[string][]byte `json:"config"`
-//}
-
+// GetDexConnectors returns the dex connectors for Dex connector controller
 func GetDexConnectors(ctx context.Context, k8sClient client.Client) (map[string]interface{}, error) {
 	secrets := &v1.SecretList{}
 	if err := k8sClient.List(ctx, secrets, client.InNamespace(types.DefaultKubeVelaNS),
--- pkg/apiserver/rest/utils/reg.go
+++ pkg/apiserver/rest/utils/reg.go
@@ -6,6 +6,7 @@ import (
 	"github.com/heroku/docker-registry-client/registry"
 )
 
+// Registry is the metadata for an image registry.
 type Registry struct {
 	Registry string `json:"registry"`
 	Username string `json:"username"`
@@ -13,11 +14,13 @@ type Registry struct {
 	Email    string `json:"email"`
 }
 
+// Image is the metadata for an image.
 type Image struct {
 	Name string `json:"name"`
 	Tag  string `json:"tag"`
 }
 
+// GetPrivateImages returns a list of private images.
 func (r Registry) GetPrivateImages() ([]Image, error) {
 	url := r.Registry
 	username := r.Username
