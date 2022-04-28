ca470045291a4a87ef95d761f6f719bbc9c555ba
楚岳 <wangyike.wyk@alibaba-inc.com>
2022-04-28 12:13:17 +0800

    test int
    Signed-off-by: 楚岳 <wangyike.wyk@alibaba-inc.com>

--- references/cli/addon_test.go
+++ references/cli/addon_test.go
@@ -77,6 +77,13 @@ func TestParseMap(t *testing.T) {
 			},
 			nilError: true,
 		},
+		{
+			args: []string{"replicas=3"},
+			res: map[string]interface{}{
+				"replicas": int64(3),
+			},
+			nilError: true,
+		},
 	}
 	for _, s := range testcase {
 		r, err := parseAddonArgsToMap(s.args)
