8d99a35f01191217966258b311a282a5a0ddcfae
Afzal Ansari <afzal442@gmail.com>
2022-05-09 08:53:45 +0530

    Fix: Refactors shell to have double quotes format (#3830)
    * refactors double quote to prevent globbing and word splitting e2e sh
    
    Signed-off-by: afzal442 <afzal442@gmail.com>
    
    * refactors e2e_core sh
    
    Signed-off-by: afzal442 <afzal442@gmail.com>
    
    * refactors cleanup sh
    
    Signed-off-by: afzal442 <afzal442@gmail.com>
    
    * refactors header-chk sh
    
    Signed-off-by: afzal442 <afzal442@gmail.com>
    
    * refactors installdef sh
    
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- hack/e2e/end_e2e_core.sh
+++ hack/e2e/end_e2e_core.sh
@@ -6,16 +6,16 @@ echo "${DOCKER_DIR}"
 docker exec kind-control-plane crictl exec "${CONTAINER_ID}" kill -2 1
 
 file=$DOCKER_DIR/e2e-profile.out
-echo $file
+echo "$file"
 n=1
 while [ $n -le 60 ];do
     if_exist=$(docker exec kind-control-plane sh -c "test -f $file && echo 'ok'")
-    echo $if_exist
+    echo "$if_exist"
     if [ -n  "$if_exist" ];then
-        docker exec kind-control-plane cat $file > /tmp/e2e-profile.out
+        docker exec kind-control-plane cat "$file" > /tmp/e2e-profile.out
         break
     fi
     echo file not generated yet
-    n=$(expr $n + 1)
+    n="$(expr $n + 1)"
     sleep 1
 done
