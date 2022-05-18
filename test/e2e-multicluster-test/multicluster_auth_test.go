9d6706d48ef281aa107ead0b7fc014da6bac3186
Somefive <yd219913@alibaba-inc.com>
2022-05-18 16:14:00 +0800

    Feat: vela auth gen-kubeconfig (#3911)
    * Feat: vela create kubeconfig
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    * Chore: refactor
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>

---
+++ test/e2e-multicluster-test/multicluster_auth_test.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2021 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e_multicluster_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test multicluster Auth commands", func() {
+
+	Context("Test vela auth commands", func() {
+
+		It("Test vela create kubeconfig for given user", func() {
+			outputs, err := execCommand("auth", "gen-kubeconfig", "--user", "kubevela", "--group", "kubevela:dev", "--group", "kubevela:test")
+			Expect(err).Should(Succeed())
+			Expect(outputs).Should(ContainSubstring("Certificate signing request kubevela approved"))
+		})
+
+		It("Test vela create kubeconfig for serviceaccount", func() {
+			outputs, err := execCommand("auth", "gen-kubeconfig", "--serviceaccount", "default", "-n", "vela-system")
+			Expect(err).Should(Succeed())
+			Expect(outputs).Should(ContainSubstring("ServiceAccount vela-system/default found."))
+		})
+
+	})
+
+})
