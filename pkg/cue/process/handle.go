2c85a078f9c4bf212e32203aee322724b3d3d65e
afzal442 <afzal442@gmail.com>
2022-06-02 12:39:23 +0530

    fixes lint error
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- pkg/cue/process/handle.go
+++ pkg/cue/process/handle.go
@@ -267,14 +267,14 @@ func (ctx *templateContext) BaseContextFile() (string, error) {
 func (ctx *templateContext) ExtendedContextFile() (string, error) {
 	context, err := ctx.BaseContextFile()
 	if err != nil {
-		return "", fmt.Errorf("failed to convert data to application with marshal err %v", err)
+		return "", fmt.Errorf("failed to convert data to application with marshal err %w", err)
 	}
 	var bareSecret string
 	if len(ctx.requiredSecrets) > 0 {
 		for _, s := range ctx.requiredSecrets {
 			data, err := json.Marshal(s.Data)
 			if err != nil {
-				return "", fmt.Errorf("failed to convert data %v to application with marshal err %v", data, err)
+				return "", fmt.Errorf("failed to convert data %v to application with marshal err %w", data, err)
 			}
 			bareSecret += s.ContextName + ":" + string(data) + "\n"
 		}
