48e3d3ac98750f92a01c87a7da002389f694d239
HanMengnan <1448189829@qq.com>
2022-06-09 16:04:17 +0800

    Feat(#3905): add the feature that views in addon are installed Independently.
    separate the deployment of views in addon and deploy them separately instead of nested in application.
    
    see the issue for details
    
    Refs #3905
    
    Signed-off-by: SiegeLion <1448189829@qq.com>

--- pkg/addon/addon.go
+++ pkg/addon/addon.go
@@ -94,6 +94,9 @@ const (
 	// DefSchemaName is the addon definition schemas dir name
 	DefSchemaName string = "schemas"
 
+	// ViewDirName is the addon views dir name
+	ViewDirName string = "views"
+
 	// AddonParameterDataKey is the key of parameter in addon args secrets
 	AddonParameterDataKey string = "addonParameterDataKey"
 
@@ -191,7 +194,7 @@ type Pattern struct {
 }
 
 // Patterns is the file pattern that the addon should be in
-var Patterns = []Pattern{{Value: ReadmeFileName}, {Value: MetadataFileName}, {Value: TemplateFileName}, {Value: ParameterFileName}, {IsDir: true, Value: ResourcesDirName}, {IsDir: true, Value: DefinitionsDirName}, {IsDir: true, Value: DefSchemaName}}
+var Patterns = []Pattern{{Value: ReadmeFileName}, {Value: MetadataFileName}, {Value: TemplateFileName}, {Value: ParameterFileName}, {IsDir: true, Value: ResourcesDirName}, {IsDir: true, Value: DefinitionsDirName}, {IsDir: true, Value: DefSchemaName}, {IsDir: true, Value: ViewDirName}}
 
 // GetPatternFromItem will check if the file path has a valid pattern, return empty string if it's invalid.
 // AsyncReader is needed to calculate relative path
@@ -306,6 +309,7 @@ func GetInstallPackageFromReader(r AsyncReader, meta *SourceMeta, uiData *UIData
 		TemplateFileName: readTemplate,
 		ResourcesDirName: readResFile,
 		DefSchemaName:    readDefSchemaFile,
+		ViewDirName:      readViewFile,
 	}
 	ptItems := ClassifyItemByPattern(meta, r)
 
@@ -405,6 +409,16 @@ func readDefFile(a *UIData, reader AsyncReader, readPath string) error {
 	default:
 		// skip other file formats
 	}
+	return nil
+}
+
+// readViewFile read single view file
+func readViewFile(a *InstallPackage, reader AsyncReader, readPath string) error {
+	b, err := reader.ReadFile(readPath)
+	if err != nil {
+		return err
+	}
+	a.Views = append(a.Views, ElementFile{Data: b, Name: filepath.Base(readPath)})
 	return nil
 }
 
@@ -809,6 +823,19 @@ func RenderDefinitionSchema(addon *InstallPackage) ([]*unstructured.Unstructured
 	return schemaConfigmaps, nil
 }
 
+// RenderViews will render views in addons.
+func RenderViews(addon *InstallPackage) ([]*unstructured.Unstructured, error) {
+	views := make([]*unstructured.Unstructured, 0)
+	for _, view := range addon.Views {
+		obj, err := renderCueView(view)
+		if err != nil {
+			return nil, err
+		}
+		views = append(views, obj)
+	}
+	return views, nil
+}
+
 func allocateDomainForAddon(ctx context.Context, k8sClient client.Client) ([]ObservabilityEnvironment, error) {
 	secrets, err := multicluster.ListExistingClusterSecrets(ctx, k8sClient)
 	if err != nil {
@@ -949,6 +976,16 @@ func renderSchemaConfigmap(elem ElementFile) (*unstructured.Unstructured, error)
 		ObjectMeta: metav1.ObjectMeta{Namespace: types.DefaultKubeVelaNS, Name: strings.Split(elem.Name, ".")[0]},
 		Data: map[string]string{
 			types.UISchema: string(jsonData),
+		}}
+	return util.Object2Unstructured(cm)
+}
+
+func renderCueView(elem ElementFile) (*unstructured.Unstructured, error) {
+	cm := v1.ConfigMap{
+		TypeMeta:   metav1.TypeMeta{APIVersion: "v1", Kind: "ConfigMap"},
+		ObjectMeta: metav1.ObjectMeta{Namespace: types.DefaultKubeVelaNS, Name: strings.Split(elem.Name, ".")[0]},
+		Data: map[string]string{
+			types.VelaQLTemplate: elem.Data,
 		}}
 	return util.Object2Unstructured(cm)
 }
@@ -1248,6 +1285,11 @@ func (h *Installer) dispatchAddonResource(addon *InstallPackage) error {
 		return errors.Wrap(err, "render addon definitions' schema fail")
 	}
 
+	views, err := RenderViews(addon)
+	if err != nil {
+		return errors.Wrap(err, "render addon views fail")
+	}
+
 	if err := passDefInAppAnnotation(defs, app); err != nil {
 		return errors.Wrapf(err, "cannot pass definition to addon app's annotation")
 	}
@@ -1267,6 +1309,14 @@ func (h *Installer) dispatchAddonResource(addon *InstallPackage) error {
 	for _, schema := range schemas {
 		addOwner(schema, app)
 		err = h.apply.Apply(h.ctx, schema, apply.DisableUpdateAnnotation())
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, view := range views {
+		addOwner(view, app)
+		err = h.apply.Apply(h.ctx, view, apply.DisableUpdateAnnotation())
 		if err != nil {
 			return err
 		}
