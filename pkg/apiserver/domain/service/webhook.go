dd9a5ecd33d26129121a810718c32ab83bc34410
qingsliu <lqs429521992@qq.com>
2022-06-20 20:48:50 +0800

    Merge branch 'release-1.4'

--- pkg/apiserver/domain/service/webhook.go
+++ pkg/apiserver/domain/service/webhook.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/emicklei/go-restful/v3"
@@ -425,7 +426,8 @@ func (j *jfrogHandlerImpl) handle(ctx context.Context, webhookTrigger *model.App
 	if err != nil {
 		return nil, err
 	}
-	image := fmt.Sprintf("%s/%s:%s", jfrogReq.Data.RepoKey, jfrogReq.Data.ImageName, jfrogReq.Data.Tag)
+	pathArray := strings.Split(jfrogReq.Data.Path,"/")
+	image := fmt.Sprintf("%s/%s:%s", jfrogReq.Data.RepoKey, strings.Join(pathArray[:len(pathArray)-2],"/"), jfrogReq.Data.Tag)
 	if jfrogReq.Data.URL != "" {
 		image = fmt.Sprintf("%s/%s", jfrogReq.Data.URL, image)
 	}
@@ -441,7 +443,7 @@ func (j *jfrogHandlerImpl) handle(ctx context.Context, webhookTrigger *model.App
 		TriggerType:  apisv1.TriggerTypeWebhook,
 		Force:        true,
 		ImageInfo: &model.ImageInfo{
-			Type: model.PayloadTypeHarbor,
+			Type: model.PayloadTypeJFrog,
 			Resource: &model.ImageResource{
 				Digest: jfrogReq.Data.Digest,
 				Tag:    jfrogReq.Data.Tag,
