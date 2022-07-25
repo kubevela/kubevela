e2031c1a7fefcfb227163964efb8930a894ed058
barnettZQG <barnett.zqg@gmail.com>
2022-07-25 11:13:04 +0800

    Feat: support to init the roles of the user who login by dex (#4431)
    * Feat: support to init the roles of the user which login by dex
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>
    
    * Fix: add the comment
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>
    
    * Fix: make the sub string to lower
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- pkg/apiserver/domain/model/system_info.go
+++ pkg/apiserver/domain/model/system_info.go
@@ -32,10 +32,17 @@ const (
 // SystemInfo systemInfo model
 type SystemInfo struct {
 	BaseModel
-	InstallID        string        `json:"installID"`
-	EnableCollection bool          `json:"enableCollection"`
-	LoginType        string        `json:"loginType"`
-	StatisticInfo    StatisticInfo `json:"statisticInfo,omitempty"`
+	InstallID              string        `json:"installID"`
+	EnableCollection       bool          `json:"enableCollection"`
+	StatisticInfo          StatisticInfo `json:"statisticInfo,omitempty"`
+	LoginType              string        `json:"loginType"`
+	DexUserDefaultProjects []ProjectRef  `json:"projects"`
+}
+
+// ProjectRef set the project name and roles
+type ProjectRef struct {
+	Name  string   `json:"name"`
+	Roles []string `json:"roles"`
 }
 
 // UpdateDexConfig update dex config
