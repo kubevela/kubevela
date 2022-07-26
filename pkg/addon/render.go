b43811431102cc4ffb5215587e4f22ad2d3e57af
Charlie Chiang <charlie_c_0129@outlook.com>
2022-07-26 21:29:38 +0800

    Feat: update vela init to not show warnings
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- pkg/addon/render.go
+++ pkg/addon/render.go
@@ -191,12 +191,12 @@ func generateAppFramework(addon *InstallPackage, parameters map[string]interface
 	}
 
 	if app.Name != "" && app.Name != addonutil.Addon2AppName(addon.Name) {
-		klog.Warningf("Application name %s will be overwritten with %s. Consider removing app name in template.", app.Name, addonutil.Addon2AppName(addon.Name))
+		klog.Warningf("Application name %s will be overwritten with %s. Consider removing metadata.name in template.", app.Name, addonutil.Addon2AppName(addon.Name))
 	}
 	app.Name = addonutil.Addon2AppName(addon.Name)
 
 	if app.Namespace != "" && app.Namespace != types.DefaultKubeVelaNS {
-		klog.Warningf("Namespace %s will be overwritten with %s. Consider removing namespace in template.", app.Namespace, types.DefaultKubeVelaNS)
+		klog.Warningf("Namespace %s will be overwritten with %s. Consider removing metadata.namespace in template.", app.Namespace, types.DefaultKubeVelaNS)
 	}
 	// force override the namespace defined vela with DefaultVelaNS. this value can be modified by Env
 	app.SetNamespace(types.DefaultKubeVelaNS)
