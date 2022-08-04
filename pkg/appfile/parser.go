9592c71dd00730a63b654581b6bfd9325886ca7a
Charlie Chiang <charlie_c_0129@outlook.com>
2022-08-05 01:03:33 +0800

    Style: change err msg
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>
    (cherry picked from commit 2bb5c0245ab070bb4036d9da331fd888bd392031)

--- pkg/appfile/parser.go
+++ pkg/appfile/parser.go
@@ -366,7 +366,7 @@ func (p *Parser) parsePoliciesFromRevision(ctx context.Context, af *Appfile) (er
 	}
 	for _, policy := range af.Policies {
 		if policy.Properties == nil {
-			return fmt.Errorf("policy name %s must not have empty properties", policy.Name)
+			return fmt.Errorf("policy %s named %s must not have empty properties", policy.Type, policy.Name)
 		}
 		switch policy.Type {
 		case v1alpha1.GarbageCollectPolicyType:
@@ -394,7 +394,7 @@ func (p *Parser) parsePolicies(ctx context.Context, af *Appfile) (err error) {
 	}
 	for _, policy := range af.Policies {
 		if policy.Properties == nil {
-			return fmt.Errorf("policy name %s must not have empty properties", policy.Name)
+			return fmt.Errorf("policy %s named %s must not have empty properties", policy.Type, policy.Name)
 		}
 		switch policy.Type {
 		case v1alpha1.GarbageCollectPolicyType:
