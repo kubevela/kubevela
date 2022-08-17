ee4f62950de3f539825922941405f338e2147ea7
Charlie Chiang <charlie_c_0129@outlook.com>
2022-08-17 20:30:45 +0800

    Test: add test
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

---
+++ pkg/utils/compression/zstd_test.go
@@ -0,0 +1,37 @@
+/*
+Copyright 2022 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package compression
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestZstdCompression(t *testing.T) {
+	obj := v1.ConfigMap{
+		Data: map[string]string{"1234": "5678"},
+	}
+
+	str, err := ZstdObjectToString(obj)
+	assert.NoError(t, err)
+	objOut := v1.ConfigMap{}
+	err = UnZstdStringToObject(str, &objOut)
+	assert.NoError(t, err)
+	assert.Equal(t, obj, objOut)
+}
