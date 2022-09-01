50b99e613c8461cdfcab9654c0afb7b1cfc46c7b
Qiaozp <qiaozhongpei.qzp@alibaba-inc.com>
2022-09-01 11:33:54 +0800

    fix multicluster test
    Signed-off-by: Qiaozp <qiaozhongpei.qzp@alibaba-inc.com>

--- test/e2e-multicluster-test/multicluster_standalone_test.go
+++ test/e2e-multicluster-test/multicluster_standalone_test.go
@@ -116,10 +116,13 @@ var _ = Describe("Test multicluster standalone scenario", func() {
 
 		deploy := readFile("deployment.yaml")
 		Expect(k8sClient.Create(hubCtx, deploy)).Should(Succeed())
+		WaitObject(hubCtx, deploy)
 		workflow := readFile("workflow-suspend.yaml")
 		Expect(k8sClient.Create(hubCtx, workflow)).Should(Succeed())
+		WaitObject(hubCtx, workflow)
 		policy := readFile("policy-zero-replica.yaml")
 		Expect(k8sClient.Create(hubCtx, policy)).Should(Succeed())
+		WaitObject(hubCtx, policy)
 		app := readFile("app-with-publish-version.yaml")
 		Expect(k8sClient.Create(hubCtx, app)).Should(Succeed())
 		appKey := client.ObjectKeyFromObject(app)
@@ -361,3 +364,10 @@ var _ = Describe("Test multicluster standalone scenario", func() {
 		Expect(k8sClient.Get(hubCtx, key, &corev1.ConfigMap{})).Should(Satisfy(errors.IsNotFound))
 	})
 })
+
+func WaitObject(ctx context.Context, un *unstructured.Unstructured) {
+	var obj client.Object
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(un), obj)).Should(Succeed())
+	}, 10*time.Second).Should(Succeed())
+}
