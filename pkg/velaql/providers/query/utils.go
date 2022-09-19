388c3d68b982d7482b4c5edf5fd36bc337eb0a69
barnettZQG <barnett.zqg@gmail.com>
2022-09-19 10:23:10 +0800

    Fix: query the resource duplicately (#4714)
    * Fix: query the resource duplicately
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>
    
    * Fix: add an e2e test case
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- pkg/velaql/providers/query/utils.go
+++ pkg/velaql/providers/query/utils.go
@@ -23,6 +23,8 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"github.com/oam-dev/kubevela/pkg/cue/model/value"
+
+	"github.com/oam-dev/kubevela/pkg/oam"
 	querytypes "github.com/oam-dev/kubevela/pkg/velaql/providers/query/types"
 )
 
@@ -61,11 +63,21 @@ func buildResourceArray(res querytypes.AppliedResource, parent, node *querytypes
 
 func buildResourceItem(res querytypes.AppliedResource, workload querytypes.Workload, object unstructured.Unstructured) querytypes.ResourceItem {
 	return querytypes.ResourceItem{
-		Cluster:        res.Cluster,
-		Workload:       workload,
-		Component:      res.Component,
-		Object:         object,
-		PublishVersion: res.PublishVersion,
-		DeployVersion:  res.DeployVersion,
+		Cluster:   res.Cluster,
+		Workload:  workload,
+		Component: res.Component,
+		Object:    object,
+		PublishVersion: func() string {
+			if object.GetAnnotations()[oam.AnnotationPublishVersion] != "" {
+				return object.GetAnnotations()[oam.AnnotationPublishVersion]
+			}
+			return res.PublishVersion
+		}(),
+		DeployVersion: func() string {
+			if object.GetAnnotations()[oam.AnnotationDeployVersion] != "" {
+				return object.GetAnnotations()[oam.AnnotationDeployVersion]
+			}
+			return res.DeployVersion
+		}(),
 	}
 }
