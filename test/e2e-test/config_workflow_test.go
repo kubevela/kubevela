175a15e1004673b3f3675603bd270924b68114d4
barnettZQG <barnett.zqg@gmail.com>
2022-10-17 22:52:06 +0800

    Fix: change the yaml
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- test/e2e-test/config_workflow_test.go
+++ test/e2e-test/config_workflow_test.go
@@ -80,27 +80,26 @@ metadata:
 spec:
   components: []
   workflow:
-	steps:
-	- name: write-config
-	  type: create-config
-	  properties:
-	  	name: test
-		config: 
-		  key1: value1
-		  key2: 2
-		  key3: true
-		  key4: 
-		    key5: value5
-	- name: read-config
-	  type: read-config
-	  properties:
-	  	name: test
-	  outputs:
-	  - fromKey: config
-	    name: read-config
-
-	- name: delete-config
-	  type: delete-config
-	  properties:
-	  	name: test
+    steps:
+    - name: write-config
+      type: create-config
+      properties:
+        name: test
+        config: 
+          key1: value1
+          key2: 2
+          key3: true
+          key4: 
+            key5: value5
+    - name: read-config
+      type: read-config
+      properties:
+        name: test
+      outputs:
+      - fromKey: config
+        name: read-config
+    - name: delete-config
+      type: delete-config
+      properties:
+        name: test
 `
