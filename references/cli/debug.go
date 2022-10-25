a05ea109bc94e177fe251bc174b7ab54d57f92fd
Tianxin Dong <dongtianxin.tx@alibaba-inc.com>
2022-10-25 16:39:38 +0800

    Fix: add sub step in vela workflow logs (#4915)
    * Fix: add sub step in vela workflow logs
    
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>
    
    * fix lint
    
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>
    
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>
    Signed-off-by: tangoliu <ltg1019@163.com>

--- references/cli/debug.go
+++ references/cli/debug.go
@@ -261,6 +261,7 @@ func wrapStepName(step workflowv1alpha1.StepStatus) string {
 }
 
 func unwrapStepName(step string) string {
+	step = strings.TrimPrefix(step, "  ")
 	switch {
 	case strings.HasPrefix(step, emojiSucceed):
 		return strings.TrimPrefix(step, emojiSucceed)
