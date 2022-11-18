a9c84c1420501f41914d071fc46aca8a5b663c4a
wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>
2022-11-18 16:22:14 +0800

    Refactor: Use github.com/spf13/cobra to execute cmd for apiserver
    Signed-off-by: wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>

---
+++ cmd/apiserver/app/options/options.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2022 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package options
+
+import (
+	cliflag "k8s.io/component-base/cli/flag"
+
+	"github.com/oam-dev/kubevela/pkg/apiserver/config"
+)
+
+// ServerRunOptions runs a kubevela api server.
+type ServerRunOptions struct {
+	GenericServerRunOptions *config.Config
+}
+
+// NewServerRunOptions creates a new ServerRunOptions object with default parameters
+func NewServerRunOptions() *ServerRunOptions {
+	s := &ServerRunOptions{
+		GenericServerRunOptions: config.NewConfig(),
+	}
+	return s
+}
+
+// Flags returns the complete NamedFlagSets
+func (s *ServerRunOptions) Flags() (fss cliflag.NamedFlagSets) {
+	fs := fss.FlagSet("generic")
+	s.GenericServerRunOptions.AddFlags(fs, s.GenericServerRunOptions)
+	return fss
+}
