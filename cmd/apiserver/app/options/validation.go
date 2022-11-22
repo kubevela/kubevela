1530f6c24ff24351b035113a7c6658c8c139861d
JohnJan <wuzhongjian_yewu@cmss.chinamobile.com>
2022-11-22 10:08:31 +0800

    Refactor: Use github.com/spf13/cobra to execute cmd for apiserver (#5085)
    Signed-off-by: wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>
    
    Signed-off-by: wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>

--- cmd/apiserver/app/options/validation.go
+++ cmd/apiserver/app/options/validation.go
@@ -1,11 +1,11 @@
 /*
-Copyright 2021 The KubeVela Authors.
+Copyright 2022 The KubeVela Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,17 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package options
 
-import (
-	"log"
+import utilerrors "k8s.io/apimachinery/pkg/util/errors"
 
-	"github.com/oam-dev/kubevela/cmd/apiserver/app"
-)
+// Validate validates server run options, to find options' misconfiguration
+func (s *ServerRunOptions) Validate() error {
+	var errors []error
 
-func main() {
-	cmd := app.NewAPIServerCommand()
-	if err := cmd.Execute(); err != nil {
-		log.Fatalln(err)
-	}
+	errors = append(errors, s.GenericServerRunOptions.Validate()...)
+
+	return utilerrors.NewAggregate(errors)
 }
