954c5ede3feaab29963652c6ac38e34583ab186b
barnettZQG <barnett.zqg@gmail.com>
2023-01-05 15:03:33 +0800

    Fix: use the klog/v2 package
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- pkg/utils/app/app.go
+++ pkg/utils/app/app.go
@@ -24,7 +24,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	k8stypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	apicommon "github.com/oam-dev/kubevela/apis/core.oam.dev/common"
