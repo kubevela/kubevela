5d6848b1c6891597087d36e8f2a82cbaa9f9b513
Jianbo Sun <jianbo.sjb@alibaba-inc.com>
2023-01-06 17:45:50 +0800

    Chore: update makefile with velaql in make def-install (#5285)
    Signed-off-by: Jianbo Sun <jianbo.sjb@alibaba-inc.com>
    
    Signed-off-by: Jianbo Sun <jianbo.sjb@alibaba-inc.com>

--- hack/utils/installdefinition.sh
+++ hack/utils/installdefinition.sh
@@ -4,7 +4,7 @@ DEF_PATH="charts/vela-core/templates/defwithtemplate"
 function check_install() {
   res=`kubectl get namespace -A | grep vela-system`
   if [ -n "$res" ];then
-    echo 'vela-system namespace exist'
+    echo 'checking: vela-system namespace exist'
   else
     echo 'vela-system namespace do not exist'
     echo 'creating vela-system namespace ...'
@@ -28,5 +28,9 @@ function check_install() {
 check_install
 
 DEF_PATH="charts/vela-core/templates/definitions"
+
+check_install
+
+DEF_PATH="charts/vela-core/templates/velaql"
 
 check_install
