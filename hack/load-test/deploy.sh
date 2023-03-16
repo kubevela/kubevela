b4ef7e915f8c82863ae804f12b34b0ab3d9d312c
Somefive <yd219913@alibaba-inc.com>
2023-03-16 18:57:04 +0800

    Feat: add load testing app template (#5526)
    * Feat: add load testing apptemplate
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    * Feat: update region template
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    * Feat: use configmap instead of secret and add cleanup script
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    * enhance readme
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    ---------
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>

--- hack/load-test/deploy.sh
+++ hack/load-test/deploy.sh
@@ -4,8 +4,11 @@ BEGIN=${BEGIN:-1}
 SIZE=${SIZE:-1000}
 WORKER=${WORKER:-6}
 VERSION=${VERSION:-1}
+CLUSTER=${CLUSTER:-4}
 
 SHARD=${SHARD:-3}
+
+TEMPLATE=${TEMPLATE:-"light"}
 
 END=$(expr $BEGIN + $SIZE - 1)
 
@@ -13,15 +16,17 @@ run() {
   for i in $(seq $1 $3 $2); do
     sid=$(expr $i % $SHARD)
     v=${VERSION}
-    cat ./app-templates/light.yaml | sed 's/ID/'$i'/g' | sed 's/SHARD/'$sid'/g' | sed 's/VERSION/'$v'/g' | kubectl apply -f -
+    c=$(expr $i % $CLUSTER)
+    cat ./app-templates/$TEMPLATE.yaml | \
+      sed 's/ID/'$i'/g' | \
+      sed 's/SHARD/'$sid'/g' | \
+      sed 's/VERSION/'$v'/g' | \
+      sed 's/CLUSTER/'$c'/g' | \
+      kubectl apply -f -
     echo "worker $4: apply app $i to $sid"
   done
   echo "worker $4: done"
 }
-
-for i in $(seq 0 $(expr $SHARD - 1)); do
-  kubectl create ns load-test-$i
-done
 
 for i in $(seq 1 $WORKER); do
   run $(expr $BEGIN + $i - 1) $END $WORKER $i &
