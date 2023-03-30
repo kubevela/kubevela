1650089e8f3e2e925fb319d9d6dbd87a09e55af7
swastik959 <Sswastik959@gmail.com>
2023-03-30 17:42:47 +0530

    corrected errors
    Signed-off-by: swastik959 <Sswastik959@gmail.com>

--- apis/core.oam.dev/v1alpha1/readonly_policy_types_test.go
+++ apis/core.oam.dev/v1alpha1/readonly_policy_types_test.go
@@ -3,8 +3,11 @@ package v1alpha1
 import (
 	"testing"
 
-	"cuelang.org/go/cue"
+	
 	"cuelang.org/go/cue/cuecontext"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	
 )
 
 func TestReadOnlyPolicySpec(t *testing.T) {
@@ -38,45 +41,46 @@ func TestReadOnlyPolicySpec(t *testing.T) {
 	`
 
 	// Create a new context and compile the CUE code
-	ctx := cuecontext.New()
-	inst, err := ctx.Compile("test", cueCode)
-	if err != nil {
-		t.Fatalf("Failed to compile CUE code: %v", err)
-	}
+    ctx := cuecontext.New()
+    r := cue.Runtime{}
+    inst, err := r.Compile("test", cueCode)
+    if err != nil {
+        t.Fatalf("Failed to compile CUE code: %v", err)
+    }
 
-	// Check the consistency of the type name
-	typeName := inst.Lookup("readOnly").Lookup("type").String()
-	if typeName != ReadOnlyPolicyType {
-		t.Errorf("Inconsistent type name: got %q, want %q", typeName, ReadOnlyPolicyType)
-	}
+    // Check the consistency of the type name
+    typeName := inst.Lookup("readOnly").Lookup("type").String()
+    if typeName != ReadOnlyPolicyType {
+        t.Errorf("Inconsistent type name: got %q, want %q", typeName, ReadOnlyPolicyType)
+    }
 
-	// Check the consistency of the FindStrategy method
-	specValue := inst.Lookup("template").Lookup("parameter").Lookup("rules").Index(0)
-	spec := &ReadOnlyPolicySpec{}
-	err = specValue.Decode(spec)
-	if err != nil {
-		t.Fatalf("Failed to decode CUE value: %v", err)
-	}
+    // Check the consistency of the FindStrategy method
+    specValue := inst.Lookup("template").Lookup("parameter").Lookup("rules").Index(0)
+    spec := &ReadOnlyPolicySpec{}
+    err = specValue.Decode(spec)
+    if err != nil {
+        t.Fatalf("Failed to decode CUE value: %v", err)
+    }
 
-	manifest := &unstructured.Unstructured{}
-	manifest.SetAPIVersion("apps/v1")
-	manifest.SetKind("Deployment")
-	manifest.SetName("example-deployment")
+    manifest := &unstructured.Unstructured{}
+    manifest.SetAPIVersion("apps/v1")
+    manifest.SetKind("Deployment")
+    manifest.SetName("example-deployment")
 
-	isReadOnly := spec.FindStrategy(manifest)
-	if isReadOnly {
-		t.Errorf("Unexpected read-only strategy: got true, want false")
-	}
+    isReadOnly := spec.FindStrategy(manifest)
+    if isReadOnly {
+        t.Errorf("Unexpected read-only strategy: got true, want false")
+    }
 
-	// Add a read-only rule and check again
-	spec.Rules = append(spec.Rules, ReadOnlyPolicyRule{
-		Selector: ResourcePolicyRuleSelector{
-			ComponentTypes: []string{"my-component"},
-		},
-	})
+    // Add a read-only rule and check again
+    spec.Rules = append(spec.Rules, ReadOnlyPolicyRule{
+        Selector: ResourcePolicyRuleSelector{
+            componentTypes: []string{"my-component"},
+        },
+    })
 
-	isReadOnly = spec.FindStrategy(manifest)
-	if !isReadOnly {
-		t.Errorf("Unexpected read-only strategy: got false, want true")
-	}
+    isReadOnly = spec.FindStrategy(manifest)
+    if !isReadOnly {
+        t.Errorf("Unexpected read-only strategy: got false, want true")
+    }
 }
