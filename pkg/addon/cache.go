c941c099301c96876efd85457554cc381e2abd57
suwanliang_yewu <suwanliang_yewu@cmss.chinamobile.com>
2023-04-26 09:36:24 +0800

    Fix: The addon page displays the deleted addon
    Signed-off-by: suwanliang_yewu <suwanliang_yewu@cmss.chinamobile.com>

--- pkg/addon/cache.go
+++ pkg/addon/cache.go
@@ -341,5 +341,21 @@ func (u *Cache) listVersionRegistryUIDataAndCache(r Registry) ([]*UIData, error)
 		// we also no version key, if use get addonUIData without version will return this vale as latest data.
 		u.putVersionedUIData2Cache(r.Name, addon.Name, "latest", uiData)
 	}
+	// delete the addon has been deleted form the addonRegistryCache
+	if addonUiDatas, ok := u.versionedUIData[r.Name]; ok {
+		for k := range addonUiDatas {
+			lastInd := strings.LastIndex(k, "-")
+			var needDelete = true
+			for _, addon := range uiDatas {
+				if k[:lastInd] == addon.Name {
+					needDelete = false
+					break
+				}
+			}
+			if needDelete {
+				delete(addonUiDatas, k)
+			}
+		}
+	}
 	return uiDatas, nil
 }
