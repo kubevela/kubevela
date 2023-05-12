d19bb89ce793c7791441bbd2b7356e248f0768fa
Siege Lion <1448189829@qq.com>
2023-05-12 22:41:41 +0800

    Fix: fix the typo error in vela top (#5982)

--- references/cli/top/view/application_view.go
+++ references/cli/top/view/application_view.go
@@ -134,7 +134,7 @@ func (v *ApplicationView) Title() string {
 func (v *ApplicationView) bindKeys() {
 	v.Actions().Delete([]tcell.Key{tcell.KeyEnter})
 	v.Actions().Add(model.KeyActions{
-		tcell.KeyESC:   model.KeyAction{Description: "Exist", Action: v.app.Exist, Visible: true, Shared: true},
+		tcell.KeyESC:   model.KeyAction{Description: "Exit", Action: v.app.Exist, Visible: true, Shared: true},
 		tcell.KeyEnter: model.KeyAction{Description: "Managed Resource", Action: v.managedResourceView, Visible: true, Shared: true},
 		component.KeyN: model.KeyAction{Description: "Select Namespace", Action: v.namespaceView, Visible: true, Shared: true},
 		component.KeyY: model.KeyAction{Description: "Yaml", Action: v.yamlView, Visible: true, Shared: true},
