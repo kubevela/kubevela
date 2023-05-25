2298dd73dc02fa5752f63a533dbe71f6f0ccca6a
Somefive <yd219913@alibaba-inc.com>
2023-05-25 17:49:44 +0800

    Feat: revert ci to kind (#6035)
    * Feat: revert ci to kind
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    * feat: use internal runner
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    * fix test bug
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>
    
    ---------
    
    Signed-off-by: Somefive <yd219913@alibaba-inc.com>

--- hack/e2e/end_e2e_core.sh
+++ hack/e2e/end_e2e_core.sh
@@ -1,19 +1,20 @@
 CORE_NAME="${CORE_NAME:-kubevela}"
-CONTAINER_ID=$(docker exec k3d-k3s-default-server-0 crictl ps | grep $CORE_NAME'\s' | grep --regexp  '^.............' -o)
-DOCKER_DIR=$(docker exec k3d-k3s-default-server-0 crictl inspect --output go-template --template '{{range .info.runtimeSpec.mounts}}{{if (eq .destination "/workspace/data")}}{{.source}}{{end}}{{end}}' "${CONTAINER_ID}")
+DOCKER_CONTAINER=kind-control-plane
+CONTAINER_ID=$(docker exec $DOCKER_CONTAINER crictl ps | grep $CORE_NAME'\s' | grep --regexp  '^.............' -o)
+DOCKER_DIR=$(docker exec $DOCKER_CONTAINER crictl inspect --output go-template --template '{{range .info.runtimeSpec.mounts}}{{if (eq .destination "/workspace/data")}}{{.source}}{{end}}{{end}}' "${CONTAINER_ID}")
 echo "${CONTAINER_ID}"
 echo "${DOCKER_DIR}"
 
-docker exec k3d-k3s-default-server-0 crictl exec "${CONTAINER_ID}" kill -2 1
+docker exec $DOCKER_CONTAINER crictl exec "${CONTAINER_ID}" kill -2 1
 
 file=$DOCKER_DIR/e2e-profile.out
 echo "$file"
 n=1
 while [ $n -le 60 ];do
-    if_exist=$(docker exec k3d-k3s-default-server-0 sh -c "test -f $file && echo 'ok'")
+    if_exist=$(docker exec $DOCKER_CONTAINER sh -c "test -f $file && echo 'ok'")
     echo "$if_exist"
     if [ -n  "$if_exist" ];then
-        docker exec k3d-k3s-default-server-0 cat "$file" >> /tmp/e2e-profile.out
+        docker exec $DOCKER_CONTAINER cat "$file" >> /tmp/e2e-profile.out
         break
     fi
     echo file not generated yet
