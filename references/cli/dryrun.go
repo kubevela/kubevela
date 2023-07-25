3c45f974390884ed4c116c84cd00453f8a5bc947
Qiaozp <qiaozhongpei.qzp@alibaba-inc.com>
2023-07-25 16:34:56 +0800

    Remove deploy templte field
    Signed-off-by: Qiaozp <qiaozhongpei.qzp@alibaba-inc.com>

--- references/cli/dryrun.go
+++ references/cli/dryrun.go
@@ -439,27 +439,8 @@ import (
 	}
 	description: "A powerful and unified deploy step for components multi-cluster delivery with policies."
 }
-template: {
-	if parameter.auto == false {
-		suspend: op.#Suspend & {message: "Waiting approval to the deploy step \"\(context.stepName)\""}
-	}
-	deploy: op.#Deploy & {
-		policies:                 parameter.policies
-		parallelism:              parameter.parallelism
-		ignoreTerraformComponent: parameter.ignoreTerraformComponent
-	}
-	parameter: {
-		//+usage=If set to false, the workflow will suspend automatically before this step, default to be true.
-		auto: *true | bool
-		//+usage=Declare the policies that used for this deployment. If not specified, the components will be deployed to the hub cluster.
-		policies: *[] | [...string]
-		//+usage=Maximum number of concurrent delivered components.
-		parallelism: *5 | int
-		//+usage=If set false, this step will apply the components with the terraform workload.
-		ignoreTerraformComponent: *true | bool
-	}
-}
-`,
+// Ignore the template field for it's useless in dry-run.
+template: {}`,
 			},
 		},
 	},
