8d7ff6521cbc2c7db865f17ebe2f434192e835f9
qiaozp <47812250+chivalryq@users.noreply.github.com>
2023-08-07 14:47:07 +0800

    Revert "Feat: apply-component supports namespace (#6228)"
    This reverts commit f0357fdc8f2332102665aed624ed2b26f90c9fdd.
    
    Signed-off-by: Qiaozp <qiaozhongpei.qzp@alibaba-inc.com>

--- pkg/controller/core.oam.dev/v1beta1/application/generator.go
+++ pkg/controller/core.oam.dev/v1beta1/application/generator.go
@@ -252,7 +252,6 @@ func convertStepProperties(step *workflowv1alpha1.WorkflowStep, app *v1beta1.App
 	o := struct {
 		Component string `json:"component"`
 		Cluster   string `json:"cluster"`
-		Namespace string `json:"namespace"`
 	}{}
 	js, err := common.RawExtensionPointer{RawExtension: step.Properties}.MarshalJSON()
 	if err != nil {
@@ -260,9 +259,6 @@ func convertStepProperties(step *workflowv1alpha1.WorkflowStep, app *v1beta1.App
 	}
 	if err := json.Unmarshal(js, &o); err != nil {
 		return err
-	}
-	if len(o.Namespace) == 0 {
-		o.Namespace = app.Namespace
 	}
 
 	var componentNames []string
@@ -292,9 +288,8 @@ func convertStepProperties(step *workflowv1alpha1.WorkflowStep, app *v1beta1.App
 			c.Outputs = nil
 			c.DependsOn = nil
 			stepProperties := map[string]interface{}{
-				"value":     c,
-				"cluster":   o.Cluster,
-				"namespace": o.Namespace,
+				"value":   c,
+				"cluster": o.Cluster,
 			}
 			step.Properties = util.Object2RawExtension(stepProperties)
 			return nil
