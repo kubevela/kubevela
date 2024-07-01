5397cf56eaa0a6e855ad688912e977837a59815f
vchinda <vchinda@guidewire.com>
2024-07-01 12:30:08 +0530

    debug

--- pkg/utils/registries/secret_authenticator_test.go
+++ pkg/utils/registries/secret_authenticator_test.go
@@ -67,6 +67,7 @@ func TestAuthn(t *testing.T) {
 		expectErr     bool
 	}{
 		{
+			// DEBUG COMMENT
 			name:          "Should authenticate with correct credential",
 			imageRegistry: buildImageRegistry("dockerhub.qingcloud.com", "guest", "guest", false, false),
 			auth:          true,
