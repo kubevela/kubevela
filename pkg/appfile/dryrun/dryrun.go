f7bc6cc534c460511c4a30ac01c6eb80811b87e5
Vishal Kumar <vishal210893@gmail.com>
2025-09-11 11:04:17 +0530

    Fix: ensure application namespace is always set to the flag-provided namespace
    Signed-off-by: Vaibhav Agrawal <vaibhav.agrawal0096@gmail.com>

--- pkg/appfile/dryrun/dryrun.go
+++ pkg/appfile/dryrun/dryrun.go
@@ -109,14 +109,8 @@ func (d *Option) ValidateApp(ctx context.Context, filename string) error {
 	if err != nil {
 		return err
 	}
-
-	namespace := oamutil.GetDefinitionNamespaceWithCtx(ctx)
-
-	if namespace != "" {
-		app.SetNamespace(namespace)
-	} else if len(app.GetNamespace()) == 0 {
-		app.SetNamespace(corev1.NamespaceDefault)
-	}
+	// Always set to default namespace (change replicated from upstream PR #6855)
+	app.SetNamespace(corev1.NamespaceDefault)
 
 	app2 := app.DeepCopy()
 
