ee7f728c2773177ab450fec8a7137d04678eda2d
Chaitanyareddy0702 <chaitanyareddy0702@gmail.com>
2025-09-19 08:00:57 +0530

    Chore: Add workflow addon testing
    Signed-off-by: Chaitanyareddy0702 <chaitanyareddy0702@gmail.com>

--- test/e2e-addon-test/addon_test.go
+++ test/e2e-addon-test/addon_test.go
@@ -21,17 +21,25 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+
+	// "os"
 	"os/exec"
 	"strconv"
+
+	// "strings"
 	"time"
 
 	terraformv1beta1 "github.com/oam-dev/terraform-controller/api/v1beta1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	// "sigs.k8s.io/yaml"
+
+	workflowv1alpha1 "github.com/kubevela/workflow/api/v1alpha1"
 	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
 	"github.com/oam-dev/kubevela/pkg/oam/util"
 	"github.com/oam-dev/kubevela/pkg/utils/common"
@@ -122,6 +130,38 @@ var _ = Describe("Addon tests", func() {
 			time.Second*30, time.Millisecond*500).ShouldNot(BeNil())
 	})
 
+	// FIt("Addon generic Terraform creates a Pod via Terraform component", func() {
+	// 	By("Install generic Terraform Addon")
+	// 	output, err := exec.Command("bash", "-c", "/Users/co/co_kubevela/kubevela/bin/vela addon enable terraform").Output()
+	// 	var ee *exec.ExitError
+	// 	if errors.As(err, &ee) {
+	// 		fmt.Println("exit code error:", string(ee.Stderr))
+	// 	}
+	// 	Expect(err).Should(BeNil())
+	// 	Expect(string(output)).Should(ContainSubstring("enabled successfully"))
+
+	// 	By("Apply ComponentDefinition for k8s-pod")
+	// 	var compDef v1beta1.ComponentDefinition
+	// 	Expect(common.ReadYamlToObject("testdata/definitions/terraform_pod_component.yaml", &compDef)).Should(BeNil())
+	// 	// create or update
+	// 	Eventually(func() error { return k8sClient.Create(ctx, compDef.DeepCopy()) }, 10*time.Second, 500*time.Millisecond).Should(Succeed())
+
+	// 	By("Apply Application that provisions a Pod using Terraform")
+	// 	raw, err := os.ReadFile("testdata/app/app_terraform_pod.yaml")
+	// 	Expect(err).Should(BeNil())
+	// 	replaced := strings.ReplaceAll(string(raw), "___NAMESPACE___", namespaceName)
+	// 	var podApp v1beta1.Application
+	// 	Expect(yaml.Unmarshal([]byte(replaced), &podApp)).Should(BeNil())
+	// 	podApp.Namespace = namespaceName
+	// 	Eventually(func() error { return k8sClient.Create(ctx, podApp.DeepCopy()) }, 20*time.Second, 500*time.Millisecond).Should(Succeed())
+
+	// 	By("Verify the Pod is created")
+	// 	Eventually(func() error {
+	// 		var pod corev1.Pod
+	// 		return k8sClient.Get(ctx, client.ObjectKey{Name: "tf-sample-pod", Namespace: namespaceName}, &pod)
+	// 	}, 180*time.Second, 2*time.Second).Should(Succeed())
+	// })
+
 	PIt("Addon observability is successfully enabled", func() {
 		By("Install Addon Observability")
 		output, err := exec.Command("bash", "-c", "/tmp/vela addon enable observability domain=abc.com disk-size=20Gi").Output()
@@ -131,5 +171,42 @@ var _ = Describe("Addon tests", func() {
 		}
 		Expect(err).Should(BeNil())
 		Expect(string(output)).Should(ContainSubstring("enabled successfully"))
+	})
+
+	FIt("Addon Workflow is successfully enabled and WorkflowRun creates Deployment", func() {
+		By("Install Addon Workflow")
+		// assume addon name is 'workflow'
+		output, err := exec.Command("bash", "-c", "/Users/co/co_kubevela/kubevela/bin/vela addon enable vela-workflow").Output()
+		var ee *exec.ExitError
+		if errors.As(err, &ee) {
+			fmt.Println("exit code error:", string(ee.Stderr))
+		}
+		Expect(err).Should(BeNil())
+		Expect(string(output)).Should(ContainSubstring("enabled successfully"))
+
+		By("Apply a WorkflowRun which creates a Deployment")
+		var wr workflowv1alpha1.WorkflowRun
+		Expect(common.ReadYamlToObject("./testdata/workflow/workflowrun_nginx.yaml", &wr)).Should(BeNil())
+		// set namespace to dynamically created test namespace
+		wr.Namespace = namespaceName
+		Eventually(func() error { return k8sClient.Create(ctx, wr.DeepCopy()) }, 20*time.Second, 500*time.Millisecond).Should(Succeed())
+
+		By("Verify the Deployment is created by WorkflowRun")
+		Eventually(func() error {
+			var deploy appsv1.Deployment
+			return k8sClient.Get(ctx, client.ObjectKey{Name: "nginx", Namespace: namespaceName}, &deploy)
+		}, 180*time.Second, 2*time.Second).Should(Succeed())
+
+		By("Check the WorkflowRun reaches a terminal phase")
+		Eventually(func() error {
+			var latest workflowv1alpha1.WorkflowRun
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: wr.Name, Namespace: namespaceName}, &latest); err != nil {
+				return err
+			}
+			if latest.Status.Phase == workflowv1alpha1.WorkflowStateSucceeded || latest.Status.Phase == workflowv1alpha1.WorkflowStateFailed || latest.Status.Phase == workflowv1alpha1.WorkflowStateTerminated {
+				return nil
+			}
+			return fmt.Errorf("workflowrun not finished, current phase: %s", latest.Status.Phase)
+		}, 300*time.Second, 2*time.Second).Should(Succeed())
 	})
 })
