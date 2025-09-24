03cac4648dc1e4125021b50631a85e46c56ee789
vishal210893 <vishal210893@gmail.com>
2025-09-25 00:01:43 +0530

    Fix: return error on directory walk failure in lookupFilesInRoot
    Signed-off-by: vishal210893 <vishal210893@gmail.com>

--- cmd/core/app/color_writer.go
+++ cmd/core/app/color_writer.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package app
 
 import (
@@ -526,7 +542,7 @@ func sourceRoots() []string {
 func lookupFilesInRoot(root, base string, seen map[string]struct{}, matches *[]string) {
 	_ = filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			return nil
+			return err
 		}
 		if d.IsDir() {
 			name := d.Name()
