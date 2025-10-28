97123d9f074b015b80eed1eeb63afc73f29c769c
Ashvin Bambhaniya <ashvin.bambhaniya@improwised.com>
2025-10-28 15:31:18 +0530

    fix(addon): improve conflict message for addon definitions
    adjust comment placement and logic to ensure correct addon name display in conflict messages
    
    Signed-off-by: Ashvin Bambhaniya <ashvin.bambhaniya@improwised.com>

--- pkg/addon/utils.go
+++ pkg/addon/utils.go
@@ -466,8 +466,8 @@ func checkConflictDefs(ctx context.Context, k8sClient client.Client, defs []*uns
 			if owner.Name != appName {
 				// if addon not belong to an addon or addon name is another one, we should put them in result
 				addonName := addon.AppName2Addon(owner.Name)
+				// If owner.Name isn't an addon app name, show the owner's name directly as the addon name
 				if addonName == "" {
-					// If owner.Name isn't an addon app name, show the owner's name directly as the addon name
 					addonName = owner.Name
 				}
 				res[checkDef.GetName()] = fmt.Sprintf("definition: %s in this addon already exist in %s \n", checkDef.GetName(), addonName)
