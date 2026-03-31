d5b0e9a54d9141737e8378f7df9e64c82f3d654a
Ayush Kumar <ayushshyamkumar888@gmail.com>
2026-03-31 14:01:29 +0530

    feat: Remove unused cuex import and initialization function from helm provider
    Signed-off-by: Ayush Kumar <ayushshyamkumar888@gmail.com>

--- pkg/workflow/providers/helm/helm.go
+++ pkg/workflow/providers/helm/helm.go
@@ -17,7 +17,6 @@ limitations under the License.
 package helm
 
 import (
-	"github.com/kubevela/pkg/cue/cuex"
 	"github.com/kubevela/pkg/cue/cuex/providers"
 	cuexruntime "github.com/kubevela/pkg/cue/cuex/runtime"
 
@@ -28,13 +27,6 @@ const (
 	// ProviderName is provider name for helm
 	ProviderName = "helm"
 )
-
-func init() {
-	// Register the helm package into the upstream cuex.DefaultCompiler so that
-	// any code path using it (e.g. the webhook's ValidateCuexTemplate) can
-	// resolve "vela/helm" imports without needing providers.DefaultCompiler.
-	cuex.DefaultCompiler.Get().LoadInternalPackages(helmProvider.Package)
-}
 
 // GetTemplate returns the cue template.
 func GetTemplate() string {
