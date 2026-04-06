5cd1528a51102c010405b0336863fa2503621fb6
Emily Vu <41875226+evu725@users.noreply.github.com>
2026-04-06 02:39:56 +0000

    Feat(cli): CUE: add support for listing and showing package resources

--- references/cli/package_object.go
+++ references/cli/package_object.go
@@ -17,85 +17,122 @@ limitations under the License.
 package cli
 
 import (
-	"context"
-	"encoding/json"
-	"strings"
-
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+	"k8s.io/kubectl/pkg/util/i18n"
+	"k8s.io/kubectl/pkg/util/templates"
 
-	core "github.com/oam-dev/kubevela/apis/core.oam.dev"
-	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
 	"github.com/oam-dev/kubevela/apis/types"
-	common2 "github.com/oam-dev/kubevela/pkg/utils/common"
-	cmdutil "github.com/oam-dev/kubevela/pkg/utils/util"
-	"github.com/oam-dev/kubevela/references/common"
+	velacmd "github.com/oam-dev/kubevela/pkg/cmd"
+	cmdutil "github.com/oam-dev/kubevela/pkg/cmd/util"
+	"github.com/oam-dev/kubevela/pkg/utils/util"
 )
 
 var (
-	pkgName string
-	pkgVersion string
-	pkgFile string
-	namespace string
+	packageListExample = templates.Examples(i18n.T(`
+		# List all packages
+		vela package list`))
+
+	packageShowExample = templates.Examples(i18n.T(`
+		# Show details of a package
+		vela package show my-package`))
 )
 
-// NewPackageObjectCommand creates 'package-object' command
-func NewPackageObjectCommand(c common2.Args, order string, ioStreams cmdutil.IOStreams) *cobra.Command {
-	var isDiscover bool
+// NewPackageCommand creates the 'vela package' command
+func NewPackageCommand(f velacmd.Factory, order string, ioStreams util.IOStreams) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "package",
-		Aliases: []string{"packages"},
-		Short:   "List/get packages.",
-		Long:    "List package types installed and discover more in registry.",
-		Example: `vela package`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// parse label filter
-			if label != "" {
-				words := strings.Split(label, "=")
-				if len(words) < 2 {
-					return errors.New("label is invalid")
-				}
-				filter = createLabelFilter(words[0], words[1])
-			}
-
-			var registry Registry
-			var err error
-			if isDiscover {
-				if regURL != "" {
-					ioStreams.Infof("Listing package definition from url: %s\n", regURL)
-					registry, err = NewRegistry(context.Background(), token, "temporary-registry", regURL)
-					if err != nil {
-						return errors.Wrap(err, "creating registry err, please check registry url")
-					}
-				} else {
-					ioStreams.Infof("Listing component definition from registry: %s\n", regName)
-					registry, err = GetRegistry(regName)
-					if err != nil {
-						return errors.Wrap(err, "get registry err")
-					}
-				}
-				return PrintComponentListFromRegistry(registry, ioStreams, filter)
-			}
-			return PrintInstalledCompDef(c, ioStreams, filter)
-		},
+		Use:   "package",
+		Short: i18n.T("Manage packages."),
 		Annotations: map[string]string{
-			types.TagCommandType:  types.TypeExtension,
+			types.TagCommandType:  types.TypeAuxiliary,
 			types.TagCommandOrder: order,
 		},
 	}
-	cmd.SetOut(ioStreams.Out)
-	cmd.AddCommand(
-		NewCompGetCommand(c, ioStreams),
-	)
-	cmd.Flags().BoolVar(&isDiscover, "discover", false, "discover packages in registries")
-	cmd.PersistentFlags().StringVar(&regURL, "url", "", "specify the registry URL")
-	cmd.PersistentFlags().StringVar(&regName, "registry", DefaultRegistry, "specify the registry name")
-	cmd.PersistentFlags().StringVar(&token, "token", "", "specify token when using --url to specify registry url")
-	cmd.Flags().StringVar(&label, types.LabelArg, "", "a label to filter components, the format is `--label type=terraform`")
-	cmd.SetOut(ioStreams.Out)
+	cmd.AddCommand(NewPackageListCommand(f, ioStreams))
+	cmd.AddCommand(NewPackageShowCommand(f, ioStreams))
+	return cmd
+}
+
+// NewPackageListCommand creates the 'vela package list' command
+func NewPackageListCommand(f velacmd.Factory, ioStreams util.IOStreams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "list",
+		Short:   i18n.T("List packages."),
+		Example: packageListExample,
+		Annotations: map[string]string{
+			types.TagCommandType: types.TypeAuxiliary,
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			objs := &unstructured.UnstructuredList{}
+			objs.SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   "cue.oam.dev",
+				Version: "v1alpha1",
+				Kind:    "PackageList",
+			})
+			cmdutil.CheckErr(f.Client().List(cmd.Context(), objs))
+
+			table := newUITable()
+			table.AddRow("NAME", "PATH", "DESCRIPTION")
+			for _, pkg := range objs.Items {
+				name := pkg.GetName()
+				path, _, _ := unstructured.NestedString(pkg.Object, "spec", "path")
+				description, _, _ := unstructured.NestedString(pkg.Object, "spec", "description")
+				table.AddRow(name, path, description)
+			}
+			ioStreams.Info(table.String())
+		},
+	}
+	return cmd
+}
+
+// NewPackageShowCommand creates the 'vela package show <name>' command
+func NewPackageShowCommand(f velacmd.Factory, ioStreams util.IOStreams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "show <name>",
+		Short:   i18n.T("Show details of a package."),
+		Example: packageShowExample,
+		Args:    cobra.ExactArgs(1),
+		Annotations: map[string]string{
+			types.TagCommandType: types.TypeAuxiliary,
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			pkg := &unstructured.Unstructured{}
+			pkg.SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   "cue.oam.dev",
+				Version: "v1alpha1",
+				Kind:    "Package",
+			})
+			cmdutil.CheckErr(f.Client().Get(cmd.Context(), k8stypes.NamespacedName{Name: args[0]}, pkg))
+			name := pkg.GetName()
+
+			description, _, _ := unstructured.NestedString(pkg.Object, "spec", "description")
+			path, _, _ := unstructured.NestedString(pkg.Object, "spec", "path")
+			proto, _, _ := unstructured.NestedString(pkg.Object, "spec", "provider", "protocol")
+			endpoint, _, _ := unstructured.NestedString(pkg.Object, "spec", "provider", "endpoint")
+			pkgTemplates, _, _ := unstructured.NestedStringMap(pkg.Object, "spec", "templates")
+
+			ioStreams.Infof("Name:         %s\n", name)
+			ioStreams.Infof("Scope:        Cluster\n")
+			ioStreams.Infof("Description:  %s\n", description)
+			ioStreams.Infof("\n")
+			ioStreams.Infof("# Usage:\n")
+			ioStreams.Infof("# Add this import to your CUE file to use this package:\n")
+			ioStreams.Infof("import \"%s\"\n", name)
+			ioStreams.Infof("\n")
+			ioStreams.Infof("# Source Information:\n")
+			ioStreams.Infof("Protocol:     %s\n", proto)
+			ioStreams.Infof("Endpoint:     %s\n", endpoint)
+			ioStreams.Infof("Path:         %s\n", path)
+			if len(pkgTemplates) > 0 {
+				ioStreams.Infof("\n")
+				ioStreams.Infof("# Exported Templates: (%d total)\n", len(pkgTemplates))
+				for k := range pkgTemplates {
+					ioStreams.Infof("  - %s\n", k)
+				}
+			}
+		},
+	}
 	return cmd
 }
