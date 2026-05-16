8b2f6fd153d1c4ae7a36e195834e7a590bafed45
Vanshika <vanshikaturakhia@gmail.com>
2026-05-16 22:40:07 +0530

    Feat: add safe shutdown lifecycle and Close() API to MemoryCacheStore
    Signed-off-by: Vanshika <vanshikaturakhia@gmail.com>

--- pkg/utils/cache.go
+++ pkg/utils/cache.go
@@ -22,25 +22,41 @@ import (
 	"time"
 )
 
-// memoryCache memory cache, support time expired
-type memoryCache struct {
-	data          interface{}
-	cacheDuration time.Duration
-	startTime     time.Time
+const defaultSweepInterval = 3 * time.Second
+
+// Option defines the functional option type for configuring MemoryCacheStore.
+type Option func(*MemoryCacheStore)
+
+// WithSweepInterval allows configuring a custom background sweep frequency.
+func WithSweepInterval(d time.Duration) Option {
+	return func(m *MemoryCacheStore) {
+		if d > 0 {
+			m.sweepInterval = d
+		}
+	}
 }
 
-// NewMemoryCache new memory cache instance
+// memoryCache memory cache, support time expired
+type memoryCache struct {
+	data       interface{}
+	expiration time.Time
+}
+
+// newMemoryCache new memory cache instance
 func newMemoryCache(data interface{}, cacheDuration time.Duration) *memoryCache {
-	mc := &memoryCache{data: data, cacheDuration: cacheDuration, startTime: time.Now()}
-	return mc
+	var exp time.Time
+	if cacheDuration > 0 {
+		exp = time.Now().Add(cacheDuration)
+	}
+	return &memoryCache{data: data, expiration: exp}
 }
 
 // IsExpired whether the cache data expires
-func (m *memoryCache) IsExpired() bool {
-	if m.cacheDuration <= 0 {
+func (c *memoryCache) IsExpired(now time.Time) bool {
+	if c.expiration.IsZero() {
 		return false
 	}
-	return time.Now().After(m.startTime.Add(m.cacheDuration))
+	return now.After(c.expiration)
 }
 
 // GetData get cache data
@@ -51,40 +67,69 @@ func (m *memoryCache) GetData() interface{} {
 // MemoryCacheStore a sample memory cache instance, if data set cache duration, will auto clear after timeout.
 // But, Expired cleanup is not necessarily accurate, it has a 3-second window.
 type MemoryCacheStore struct {
-	store sync.Map
+	store         sync.Map
+	cancel        context.CancelFunc
+	wg            sync.WaitGroup
+	sweepInterval time.Duration
+	closeOnce     sync.Once
 }
 
 // NewMemoryCacheStore memory cache store
-func NewMemoryCacheStore(ctx context.Context) *MemoryCacheStore {
-	mcs := &MemoryCacheStore{
-		store: sync.Map{},
+func NewMemoryCacheStore(parent context.Context, opts ...Option) *MemoryCacheStore {
+	if parent == nil {
+		parent = context.Background()
 	}
-	go mcs.run(ctx)
-	return mcs
+
+	ctx, cancel := context.WithCancel(parent)
+
+	m := &MemoryCacheStore{
+		sweepInterval: defaultSweepInterval,
+		cancel:        cancel,
+	}
+
+	for _, opt := range opts {
+		opt(m)
+	}
+
+	m.wg.Add(1)
+	go func() {
+		defer m.wg.Done()
+
+		ticker := time.NewTicker(m.sweepInterval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				now := time.Now()
+				m.store.Range(func(k, v interface{}) bool {
+					if item, ok := v.(*memoryCache); ok && item.IsExpired(now) {
+						m.store.Delete(k)
+					}
+					return true
+				})
+			}
+		}
+	}()
+
+	return m
 }
 
-func (m *MemoryCacheStore) run(ctx context.Context) {
-	ticker := time.NewTicker(time.Second * 3)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			m.store.Range(func(key, value interface{}) bool {
-				if value.(*memoryCache).IsExpired() {
-					m.store.Delete(key)
-				}
-				return true
-			})
+// Close gracefully stops the background sweeper goroutine. It is safe to call concurrently.
+func (m *MemoryCacheStore) Close() {
+	m.closeOnce.Do(func() {
+		if m.cancel != nil {
+			m.cancel()
+			m.wg.Wait()
 		}
-	}
+	})
 }
 
 // Put cache data, if cacheDuration>0, store will clear data after timeout.
 func (m *MemoryCacheStore) Put(key, value interface{}, cacheDuration time.Duration) {
-	mc := newMemoryCache(value, cacheDuration)
-	m.store.Store(key, mc)
+	m.store.Store(key, newMemoryCache(value, cacheDuration))
 }
 
 // Delete cache data from store
@@ -94,9 +139,20 @@ func (m *MemoryCacheStore) Delete(key interface{}) {
 
 // Get cache data from store, if not exist or timeout, will return nil
 func (m *MemoryCacheStore) Get(key interface{}) (value interface{}) {
-	mc, ok := m.store.Load(key)
-	if ok && !mc.(*memoryCache).IsExpired() {
-		return mc.(*memoryCache).GetData()
+	v, exists := m.store.Load(key)
+	if !exists {
+		return nil
 	}
-	return nil
+
+	item, ok := v.(*memoryCache)
+	if !ok {
+		return nil
+	}
+
+	if item.IsExpired(time.Now()) {
+		m.store.Delete(key)
+		return nil
+	}
+
+	return item.GetData()
 }
--- pkg/utils/cache_test.go
+++ pkg/utils/cache_test.go
@@ -19,60 +19,123 @@ package utils
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Test cache utils", func() {
-	It("should return false for IsExpired()", func() {
-		c := newMemoryCache("test", 10*time.Hour)
-		Expect(c.IsExpired()).Should(BeFalse())
-	})
+func TestMemoryCacheIsExpired(t *testing.T) {
+	g := NewWithT(t)
+	g.Expect(newMemoryCache("test", 10*time.Hour).IsExpired(time.Now())).Should(BeFalse())
+	g.Expect(newMemoryCache("test", 0).IsExpired(time.Now())).Should(BeFalse())
+	g.Expect(newMemoryCache("test", -1*time.Second).IsExpired(time.Now())).Should(BeFalse())
+}
 
-	It("test cache store", func() {
-		store := NewMemoryCacheStore(context.TODO())
-		store.Put("test", "test data", time.Second*2)
-		store.Put("test2", "test data", 0)
-		store.Put("test3", "test data", -1)
-		time.Sleep(3 * time.Second)
-		Expect(store.Get("test")).Should(BeNil())
-		Expect(store.Get("test2")).Should(Equal("test data"))
-		Expect(store.Get("test3")).Should(Equal("test data"))
-	})
+func TestMemoryCacheStoreTTL(t *testing.T) {
+	g := NewWithT(t)
+	store := NewMemoryCacheStore(context.TODO(), WithSweepInterval(50*time.Millisecond))
+	defer store.Close()
+	store.Put("test", "test data", time.Second*2)
+	store.Put("test2", "test data", 0)
+	store.Put("test3", "test data", -1)
+	time.Sleep(3 * time.Second)
+	g.Expect(store.Get("test")).Should(BeNil())
+	g.Expect(store.Get("test2")).Should(Equal("test data"))
+	g.Expect(store.Get("test3")).Should(Equal("test data"))
+}
 
-	It("test cache store delete key", func() {
-		store := NewMemoryCacheStore(context.TODO())
-		store.Put("test", "test data", time.Minute*2)
-		store.Delete("test")
-		Expect(store.Get("test")).Should(BeNil())
-	})
-})
+func TestMemoryCacheStoreDeleteKey(t *testing.T) {
+	g := NewWithT(t)
+	store := NewMemoryCacheStore(context.TODO())
+	defer store.Close()
+	store.Put("test", "test data", time.Minute*2)
+	store.Delete("test")
+	g.Expect(store.Get("test")).Should(BeNil())
+}
 
-var store *MemoryCacheStore
+func TestMemoryCacheStoreShutdown(t *testing.T) {
+	g := NewWithT(t)
+	store := NewMemoryCacheStore(context.TODO(), WithSweepInterval(50*time.Millisecond))
+	start := time.Now()
+	store.Close()
+	g.Expect(time.Since(start)).Should(BeNumerically("<", 2*time.Second))
+}
 
-// BenchmarkWrite
-func BenchmarkWrite(b *testing.B) {
-	store = NewMemoryCacheStore(context.TODO())
+func TestMemoryCacheStoreTTLBoundaries(t *testing.T) {
+	g := NewWithT(t)
+	store := NewMemoryCacheStore(context.TODO(), WithSweepInterval(50*time.Millisecond))
+	defer store.Close()
+
+	store.Put("soon", "value", 100*time.Millisecond)
+	store.Put("keep", "value", 0)
+
+	g.Eventually(func() interface{} {
+		return store.Get("soon")
+	}, 5*time.Second, 50*time.Millisecond).Should(BeNil())
+
+	g.Expect(store.Get("keep")).Should(Equal("value"))
+}
+
+func TestMemoryCacheStoreIdempotentClose(t *testing.T) {
+	g := NewWithT(t)
+	store := NewMemoryCacheStore(context.TODO())
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			store.Close()
+		}()
+	}
+	wg.Wait()
+
+	g.Expect(store.Get("any")).Should(BeNil())
+	store.Put("key", "val", 0)
+	g.Expect(store.Get("key")).Should(Equal("val"))
+}
+
+func TestMemoryCacheStoreConcurrency(t *testing.T) {
+	store := NewMemoryCacheStore(context.TODO(), WithSweepInterval(100*time.Millisecond))
+	defer store.Close()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			key := fmt.Sprintf("key-%d", id)
+			for j := 0; j < 50; j++ {
+				store.Put(key, j, time.Minute)
+				store.Get(key)
+				store.Delete(key + "-del")
+			}
+		}(i)
+	}
+	wg.Wait()
+}
+
+var benchStore *MemoryCacheStore
+
+func BenchmarkMemoryCacheWrite(b *testing.B) {
+	benchStore = NewMemoryCacheStore(context.TODO())
+	defer benchStore.Close()
 
 	for i := 0; i < b.N; i++ {
-		store.Put(fmt.Sprintf("%d", i), i, 0)
+		benchStore.Put(fmt.Sprintf("%d", i), i, 0)
 	}
 }
 
-// BenchmarkRead
-func BenchmarkRead(b *testing.B) {
+func BenchmarkMemoryCacheRead(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		store.Get(fmt.Sprintf("%d", i))
+		benchStore.Get(fmt.Sprintf("%d", i))
 	}
 }
 
-// BenchmarkRW
-func BenchmarkRW(b *testing.B) {
+func BenchmarkMemoryCacheRW(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		store.Put(fmt.Sprintf("%d", i), i, 1)
-		store.Get(fmt.Sprintf("%d", i))
+		benchStore.Put(fmt.Sprintf("%d", i), i, 1)
+		benchStore.Get(fmt.Sprintf("%d", i))
 	}
 }
