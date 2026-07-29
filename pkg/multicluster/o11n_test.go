6adec9597ca4eb0892c840a02042c995d78fed1b
manoj-1407 <manojkumar148700@gmail.com>
2026-07-29 05:00:20 +0000

    Style: gofmt pkg/multicluster/o11n_test.go
    Signed-off-by: manoj-1407 <manojkumar148700@gmail.com>

--- pkg/multicluster/o11n_test.go
+++ pkg/multicluster/o11n_test.go
@@ -23,10 +23,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	storagev1 "k8s.io/api/storage/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
